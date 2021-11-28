e4fb94aee484cefd51448c1aad8aa0517374e7a2
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2021-11-28 16:41:37 +0100

    Moved code out of /lib/data and removed package.

--- app/lib/widgets/settings/icon_license.dart
+++ app/lib/widgets/settings/icon_license.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sharezone_widgets/svg.dart';
-import 'package:sharezone/data/icon_licences.dart';
 import 'package:sharezone/util/launch_link.dart';
 
 class IconLicense extends StatelessWidget {
@@ -57,4 +56,64 @@ class IconListTitle extends StatelessWidget {
       },
     );
   }
+}
+
+class IconsLicenses {
+  static String freepik = "Freepik";
+  static String pixelPerfect = "Pixel Perfect";
+  static String daveGandy = "Dave Gandy";
+  static String icomoon = "Icomoon";
+  static String smashicons = "Smashicons";
+
+  static String flationURL = "https://www.flaticon.com";
+  static String freepikURL = "https://www.flaticon.com/authors/freepik";
+  static String daveGandyURL = "https://www.flaticon.com/authors/dave-gandy";
+  static String icomoonURL = "https://www.flaticon.com/authors/icomoon";
+  static String smashiconsURL = "https://www.flaticon.com/authors/smashicons";
+
+  static String licenseFlation = "Flaticon Basic License";
+  static String licenseCC = "Attribution 3.0 Unported (CC BY 3.0)";
+
+  static List<IconListTitle> icons = [
+    IconListTitle(
+        author: freepik,
+        iconPath: 'assets/icons/fire.svg',
+        url: freepikURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: freepik,
+        iconPath: 'assets/icons/professor.svg',
+        url: freepikURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: freepik,
+        iconPath: 'assets/icons/ghost.svg',
+        url: freepikURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: freepik,
+        iconPath: 'assets/icons/parents.svg',
+        url: freepikURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: freepik,
+        iconPath: 'assets/icons/students.svg',
+        url: freepikURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: pixelPerfect,
+        iconPath: 'assets/icons/instagram.svg',
+        url: flationURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: smashicons,
+        iconPath: 'assets/icons/game-controller.svg',
+        url: smashiconsURL,
+        license: licenseFlation),
+    IconListTitle(
+        author: daveGandy,
+        iconPath: 'assets/icons/email.svg',
+        url: daveGandyURL,
+        license: licenseCC),
+  ];
 }
