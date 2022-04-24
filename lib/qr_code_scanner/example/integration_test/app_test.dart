48d03d2ef5980936e9e5180adb899e24edb5f7aa
nilsreichardt <nils@reichardt.io>
2022-04-24 15:36:10 +0200

    temp

--- lib/qr_code_scanner/example/integration_test/app_test.dart
+++ lib/qr_code_scanner/example/integration_test/app_test.dart
@@ -8,6 +8,8 @@ void main() {
 
   // Before running the test, add the screenshot "qr_code.jpg" to your Android
   // emulator camera: https://stackoverflow.com/a/64922184/8358501
+  //
+  // And you need to grand the permission to access the camera.
   testWidgets(
     "scan qr code",
     (tester) async {
@@ -18,6 +20,10 @@ void main() {
 
       // await tester.pump(const Duration(seconds: 10));
 
+      await waitFor(tester, find.byKey(const Key('scan-qr-code-button-e2e')));
+
+      print("FSA");
+
       expect(
         find.text('Received QR code: https://sharez.one/pGmfH4rTQeuxXbLE6'),
         findsOneWidget,
@@ -26,8 +32,15 @@ void main() {
     // Not working at the moment. Sometimes the camera does not include the
     // screenshot and otherwise you get a 'PlatformException(error, No active
     // stream to cancel, null, null)' error.
-    skip: true,
+    skip: false,
     // At the moment only for Android the setup for this test is known.
     variant: TargetPlatformVariant.only(TargetPlatform.android),
   );
+}
+
+Future<void> waitFor(WidgetTester tester, Finder finder) async {
+  do {
+    await tester.pumpAndSettle();
+    await Future.delayed(const Duration(milliseconds: 100));
+  } while (finder.evaluate().isEmpty);
 }
