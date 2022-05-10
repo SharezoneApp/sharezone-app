936f3bbb11e8548b7aeca5f78edf74d7ff9da6c5
nilsreichardt <nils@reichardt.io>
2022-05-10 18:31:22 +0200

    Add `app_test.dart` and new `README.md`

---
+++ app/integration_test/README.md
@@ -0,0 +1,53 @@
+# Integration Tests
+Unit tests and widget tests are handy for testing individual classes, functions, or widgets. However, they generally don’t test how individual pieces work together as a whole, or capture the performance of an application running on a real device. These tasks are performed with integration tests.
+
+Integration tests are written using the [integration_test](https://github.com/flutter/flutter/tree/master/packages/integration_test) package, provided by the SDK.
+
+## How to run integration tests
+In order to run the integration tests, you need to setup a fresh Sharezone account which can be used for the integration tests. This account needs to be linked to an email address and a password.
+
+### Mobile
+You can run the integration tests using the `flutter drive` command:
+```sh
+flutter test \
+  integration_test \
+  --flavor dev \
+  --dart-define \
+  USER_1_EMAIL="YOUR@EMAIL.COM" \
+  --dart-define \
+  USER_1_PASSWORD="YOUR_PASSWORD"
+```
+
+### Web
+_Note: The integration tests are not working for the web at the moment because we need to migrate our app to null safety. Otherwise, the build will fail because of this message:_
+```
+org-dartlang-app:/app_test.dart:27:11: Error: Non-nullable variable 'dependencies' must be assigned before it can be used.
+    await dependencies.blocDependencies.auth.signOut();
+          ^^^^^^^^^^^^
+org-dartlang-app:/app_test.dart:34:30: Error: Non-nullable variable 'dependencies' must be assigned before it can be used.
+          beitrittsversuche: dependencies.beitrittsversuche,
+                             ^^^^^^^^^^^^
+org-dartlang-app:/app_test.dart:35:29: Error: Non-nullable variable 'dependencies' must be assigned before it can be used.
+          blocDependencies: dependencies.blocDependencies,
+                            ^^^^^^^^^^^^
+org-dartlang-app:/app_test.dart:36:28: Error: Non-nullable variable 'dependencies' must be assigned before it can be used.
+          dynamicLinkBloc: dependencies.dynamicLinkBloc,
+                           ^^^^^^^^^^^^
+```
+
+To get started testing in a web browser, download [ChromeDriver](https://chromedriver.chromium.org/downloads).
+
+Launch WebDriver, for example:
+```sh
+chromedriver --port=4444
+```
+
+And then run the following command in a different process:
+```sh
+flutter test \
+  integration_test
+  --flavor dev
+  -d web-server
+```
+
+
---
+++ app/integration_test/app_test.dart
@@ -0,0 +1,88 @@
+// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:sharezone/main/run_app.dart';
+import 'package:sharezone/main/sharezone.dart';
+
+/// The credentials for user used in the integration tests.
+class _UserCredentials {
+  const _UserCredentials({
+    @required this.email,
+    @required this.password,
+  });
+
+  /// The email address of the user.
+  final String email;
+
+  /// The password of the user.
+  final String password;
+}
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  AppDependencies dependencies;
+  _UserCredentials user1;
+
+  setUpAll(() async {
+    dependencies = await initializeFlutterApp();
+  });
+
+  setUp(() async {
+    // Credentials are passed via environment variables. See "README.md" how to
+    // pass the them correctly.
+    user1 = _UserCredentials(
+      email: const String.fromEnvironment('USER_1_EMAIL'),
+      password: const String.fromEnvironment('USER_1_PASSWORD'),
+    );
+
+    // We should ensure that the user is logged out before running a test, to
+    // have fresh start.
+    await dependencies.blocDependencies.auth.signOut();
+  });
+
+  Future<void> _pumpSharezoneApp(WidgetTester tester) async {
+    await tester.pumpWidget(
+      Sharezone(
+        beitrittsversuche: dependencies.beitrittsversuche,
+        blocDependencies: dependencies.blocDependencies,
+        dynamicLinkBloc: dependencies.dynamicLinkBloc,
+      ),
+    );
+  }
+
+  group('Authentication', () {
+    testWidgets('User should be able to sign in', (tester) async {
+      await _pumpSharezoneApp(tester);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+
+      await tester.tap(find.byKey(const Key('go-to-login-button-E2E')));
+      await tester.pumpAndSettle();
+
+      await tester.enterText(
+        find.byKey(const Key('email-text-field-E2E')),
+        user1.email,
+      );
+      await tester.enterText(
+        find.byKey(const Key('password-text-field-E2E')),
+        user1.password,
+      );
+
+      await tester.tap(find.byKey(const Key('login-button-E2E')));
+      await tester.pumpAndSettle();
+
+      expect(
+        find.byKey(const Key('dashboard-appbar-title-E2E')),
+        findsOneWidget,
+      );
+    });
+  });
+}
