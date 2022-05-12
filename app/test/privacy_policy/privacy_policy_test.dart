a41e817d76336e3f6d7dd276fea15c69ad8e5524
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2022-05-12 20:43:12 +0200

    wip widget test

---
+++ app/test/privacy_policy/privacy_policy_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sharezone/onboarding/sign_up/pages/new_privacy_policy_page.dart';
+import 'package:sharezone_utils/random_string.dart';
+
+void main() {
+  group('privacy policy page', () {
+    group('table of contents', () {
+      testWidgets('highlights first section if its at the start of the text',
+          (tester) async {
+        tester.binding.window.physicalSizeTestValue = Size(1920, 1080);
+        addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+
+        final text = '''
+Lorem ipsum dolor sit amet.
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+# Inhaltsverzeichnis
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+''';
+
+        await tester.pumpWidget(
+          wrapWithScaffold(NewPrivacyPolicy(
+            content: text,
+            documentSections: [
+              DocumentSection('inhaltsverzeichnis', 'Inhaltsverzeichnis'),
+            ],
+          )),
+        );
+      });
+    });
+  });
+}
+
+Widget wrapWithScaffold(Widget privacyPolicyPage) {
+  return MaterialApp(home: privacyPolicyPage);
+}
