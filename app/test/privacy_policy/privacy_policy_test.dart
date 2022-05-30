79554bf7c95ffb2cbb44b74634dcc38f87622dba
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2022-05-30 16:24:34 +0200

    Remove skip

--- app/test/privacy_policy/privacy_policy_test.dart
+++ app/test/privacy_policy/privacy_policy_test.dart
@@ -4,15 +4,17 @@ import 'package:sharezone/onboarding/sign_up/pages/privacy_policy/new_privacy_po
 import 'package:sharezone_utils/random_string.dart';
 
 void main() {
-  group('privacy policy page', () {
-    group('table of contents', () {
-      testWidgets('highlights first section if its at the start of the text',
-          (tester) async {
-        tester.binding.window.physicalSizeTestValue = Size(1920, 1080);
-        tester.binding.window.devicePixelRatioTestValue = 1.0;
-        addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+  group(
+    'privacy policy page',
+    () {
+      group('table of contents', () {
+        testWidgets('highlights first section if its at the start of the text',
+            (tester) async {
+          tester.binding.window.physicalSizeTestValue = Size(1920, 1080);
+          tester.binding.window.devicePixelRatioTestValue = 1.0;
+          addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
 
-        final text = '''
+          final text = '''
 Lorem ipsum dolor sit amet.
 
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
@@ -22,19 +24,18 @@ Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 ''';
 
-        await tester.pumpWidget(
-          wrapWithScaffold(NewPrivacyPolicy(
-            content: text,
-            documentSections: [
-              DocumentSection('inhaltsverzeichnis', 'Inhaltsverzeichnis'),
-            ],
-          )),
-        );
+          await tester.pumpWidget(
+            wrapWithScaffold(NewPrivacyPolicy(
+              content: text,
+              documentSections: [
+                DocumentSection('inhaltsverzeichnis', 'Inhaltsverzeichnis'),
+              ],
+            )),
+          );
+        });
       });
-    });
-  },
-      skip:
-          'Doesnt work right now, delete before merging if this cant be fixed');
+    },
+  );
 }
 
 Widget wrapWithScaffold(Widget privacyPolicyPage) {
