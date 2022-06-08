ce1d708c6221549a12b9beaf7c23d546c36180bb
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2022-06-08 18:13:40 +0200

    Add two widget tests for section highlighting.

--- app/test/privacy_policy/privacy_policy_test.dart
+++ app/test/privacy_policy/privacy_policy_test.dart
@@ -7,21 +7,22 @@ void main() {
   group(
     'privacy policy page',
     () {
+      // TODO: Consider deleting these tests later.
+      // Testing this might be done in a more e2e way since we already
+      // test the logic in unit tests.
       group('table of contents', () {
-        testWidgets('highlights first section if its at the start of the text',
+        testWidgets('highlights no section if we havent crossed any yet',
             (tester) async {
           tester.binding.window.physicalSizeTestValue = Size(1920, 1080);
           tester.binding.window.devicePixelRatioTestValue = 1.0;
           addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
 
           final text = '''
-Lorem ipsum dolor sit amet.
-
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
+${generateText(10)}
 # Inhaltsverzeichnis
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+${generateText(10)}
+
+${generateText(10)}
 ''';
 
           await tester.pumpWidget(
@@ -32,10 +33,90 @@ Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
               ],
             )),
           );
+
+          expect(
+              find.byWidgetPredicate((widget) =>
+                  widget is SectionHighlight &&
+                  widget.shouldHighlight == false),
+              findsOneWidget);
+          expect(
+              find.byWidgetPredicate((widget) =>
+                  widget is SectionHighlight && widget.shouldHighlight == true),
+              findsNothing);
+        });
+        testWidgets('highlights section if we have scrolled past it',
+            (tester) async {
+          tester.binding.window.physicalSizeTestValue = Size(1920, 1080);
+          tester.binding.window.devicePixelRatioTestValue = 1.0;
+          addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+
+          final text = '''
+Test test test
+
+test 
+
+test
+
+# Inhaltsverzeichnis
+${generateText(10)}
+
+${generateText(10)}
+${generateText(10)}
+
+
+
+${generateText(10)}
+''';
+
+          await tester.pumpWidget(
+            wrapWithScaffold(NewPrivacyPolicy(
+              content: text,
+              documentSections: [
+                DocumentSection('inhaltsverzeichnis', 'Inhaltsverzeichnis'),
+              ],
+            )),
+          );
+
+          await expectLater(find.byType(NewPrivacyPolicy),
+              matchesGoldenFile('goldens/golden_pp1.png'));
+
+          // await tester.flingFrom(Offset(0, 0), Offset(0, -100), 10000);
+          await tester.fling(
+              find.byType(PrivacyPolicyText), Offset(0, -400), 10000);
+          await tester.pumpAndSettle();
+
+          expect(
+              find.byWidgetPredicate((widget) =>
+                  widget is SectionHighlight && widget.shouldHighlight == true),
+              findsOneWidget);
+          expect(
+              find.byWidgetPredicate((widget) =>
+                  widget is SectionHighlight &&
+                  widget.shouldHighlight == false),
+              findsNothing);
         });
       });
     },
   );
+}
+
+// Used temporarily when testing so one can see what happens "on the screen" in
+// a widget test without having to use a real device / simulator to run these
+// tests.
+Future<void> generateGolden() async {
+  await expectLater(find.byType(NewPrivacyPolicy),
+      matchesGoldenFile('goldens/golden_pp2.png'));
+}
+
+String generateText(int times) {
+  return """
+Lorem ipsum dolor sit amet.
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitrsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+""" *
+      times;
 }
 
 Widget wrapWithScaffold(Widget privacyPolicyPage) {
