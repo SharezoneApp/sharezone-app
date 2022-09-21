9fcb0147dbb6826cf1c0a41a502aebe53801224a
nilsreichardt <hi@nils.re>
2022-09-21 12:04:28 +0200

    Replace "Github" with "GitHub"

--- lib/qr_code_scanner/test/flutter_test_config.dart
+++ lib/qr_code_scanner/test/flutter_test_config.dart
@@ -17,10 +17,10 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async {
     },
     config: GoldenToolkitConfiguration(
       // Due to slight differences in rendering across platforms, mostly around
-      // text, the tests will only be run on a macOS machine on Github Actions.
+      // text, the tests will only be run on a macOS machine on GitHub Actions.
       // This means that if you update the tests on a Linux or Windows machine
-      // the golden tests will not pass on Github Actions. Instead you are
-      // recommended to download the goldens directly from the failed Github
+      // the golden tests will not pass on GitHub Actions. Instead you are
+      // recommended to download the goldens directly from the failed GitHub
       // Actions job, and use those inside of your branch.
       //
       // See
