79ea0aa40372d5511c696a1e6870ba40251346c3
nilsreichardt <hi@nils.re>
2022-09-30 20:48:26 +0200

    `randomString()`

---
+++ lib/sharezone_utils/test/random_string/random_string_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sharezone_utils/random_string.dart';
+
+void main() {
+  test('randomString()', () {
+    for (int i = 0; i < 50; i++) {
+      expect(randomString(10), hasLength(10));
+    }
+  });
+}
