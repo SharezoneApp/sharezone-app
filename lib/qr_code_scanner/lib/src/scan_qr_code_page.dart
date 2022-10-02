11c6d65483c1189f239bc104416e30050a4ecf8e
nilsreichardt <hi@nils.re>
2022-10-02 16:38:04 +0200

    add "passes route settings"

--- lib/qr_code_scanner/lib/src/scan_qr_code_page.dart
+++ lib/qr_code_scanner/lib/src/scan_qr_code_page.dart
@@ -7,6 +7,7 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 /// Opens a full screen scanner to scan a QR code.
@@ -19,11 +20,14 @@ import 'package:qr_code_scanner/qr_code_scanner.dart';
 /// The [description] will be displayed above the scanning area. A [description]
 /// can an assistance for the user, like "Go to web.sharezone.net to get a QR
 /// code".
+///
+/// For testing purposes, you can pass a [controller] to mock the scanner.
 Future<String?> scanQrCode(
   BuildContext context, {
   Widget? title = const Text('Scan QR Code'),
   Widget? description,
   RouteSettings? settings,
+  MobileScannerController? controller,
 }) {
   return Navigator.push<String?>(
     context,
@@ -31,6 +35,7 @@ Future<String?> scanQrCode(
       builder: (context) => _ScanQrCodePage(
         title: title,
         description: description,
+        controller: controller,
       ),
       settings: settings,
     ),
@@ -43,6 +48,7 @@ class _ScanQrCodePage extends StatelessWidget {
     Key? key,
     required this.title,
     this.description,
+    this.controller,
   }) : super(key: key);
 
   /// The title that is displayed at the top in the [AppBar]
@@ -52,19 +58,39 @@ class _ScanQrCodePage extends StatelessWidget {
   /// the scan area.
   final Widget? description;
 
+  /// A controller that can be used to control the scanner.
+  ///
+  /// Is primarily used for testing to mock the scanner.
+  final MobileScannerController? controller;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: title,
-      ),
-      body: Scanner(
-        description: description,
-        onDetect: (qrCode) => Navigator.pop(context, qrCode),
-      ),
+    return Material(
+      child: Builder(builder: (context) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            appBarTheme: AppBarTheme(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              titleTextStyle: AppBarTheme.of(context).titleTextStyle?.copyWith(
+                    color: Colors.white,
+                  ),
+              iconTheme: IconTheme.of(context).copyWith(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          child: Scaffold(
+            extendBodyBehindAppBar: true,
+            appBar: AppBar(title: title),
+            body: Scanner(
+              controller: controller,
+              description: description,
+              onDetect: (qrCode) => Navigator.pop(context, qrCode),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
---
+++ lib/qr_code_scanner/test/scan_qr_code_page_test.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+import 'fake_mobile_scanner_controller.dart';
+
+class RouteSettingsObserver extends NavigatorObserver {
+  String? routeName;
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    routeName = route.settings.name;
+    super.didPush(route, previousRoute);
+  }
+}
+
+void main() {
+  group('scanQrCode()', () {
+    testWidgets('passes route settings', (tester) async {
+      final routeSettingsObserver = RouteSettingsObserver();
+      final controller = FakeMobileScannerController();
+      const route = 'my-route';
+
+      await tester.pumpWidget(
+        MaterialApp(
+          navigatorObservers: [routeSettingsObserver],
+          home: Scaffold(
+            body: Builder(builder: (context) {
+              return ElevatedButton(
+                onPressed: () => scanQrCode(
+                  context,
+                  controller: controller,
+                  settings: const RouteSettings(name: route),
+                ),
+                child: const Text('Scan QR Code'),
+              );
+            }),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+
+      expect(routeSettingsObserver.routeName, route);
+    });
+  });
+}
