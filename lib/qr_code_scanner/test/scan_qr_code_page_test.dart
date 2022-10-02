0d02bc73e2d33805e42598f733d172f4c709067f
nilsreichardt <hi@nils.re>
2022-10-02 16:46:18 +0200

    Add screen shot test for scanner page

--- lib/qr_code_scanner/test/scan_qr_code_page_test.dart
+++ lib/qr_code_scanner/test/scan_qr_code_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import 'fake_mobile_scanner_controller.dart';
@@ -47,6 +48,31 @@ void main() {
       await tester.tap(find.byType(ElevatedButton));
 
       expect(routeSettingsObserver.routeName, route);
+    });
+
+    testGoldens('displays scanner page as expected', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Builder(builder: (context) {
+              return ElevatedButton(
+                onPressed: () => scanQrCode(
+                  context,
+                  controller: controller,
+                ),
+                child: const Text('Scan QR Code'),
+              );
+            }),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+
+      await screenMatchesGolden(
+        tester,
+        'scanner_page',
+      );
     });
   });
 }
