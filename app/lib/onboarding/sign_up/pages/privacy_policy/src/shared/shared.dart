a5f0bd73de9b2669495ac4f81d7f302bd52c78bb
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2022-11-21 17:39:45 +0100

    Add license header.

--- app/lib/onboarding/sign_up/pages/privacy_policy/src/shared/shared.dart
+++ app/lib/onboarding/sign_up/pages/privacy_policy/src/shared/shared.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 export 'currently_read_threshold.dart';
 export 'privacy_policy.dart';
 export 'privacy_policy_end_section.dart';
--- app/test/privacy_policy/helper.dart
+++ app/test/privacy_policy/helper.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:sharezone/onboarding/sign_up/pages/privacy_policy/src/privacy_policy_src.dart';
 
