5e2e619c682bca9cf30f356f9feef55742e01550
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-01-23 18:23:19 +0100

    Add license headers to newly added files.

--- bin/source_of_truth/get_sot_cmd.sh
+++ bin/source_of_truth/get_sot_cmd.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+# Licensed under the EUPL-1.2-or-later.
+#
+# You may obtain a copy of the Licence at:
+# https://joinup.ec.europa.eu/software/page/eupl
+#
+# SPDX-License-Identifier: EUPL-1.2
+
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 commands_file="$script_dir/commands_source_of_truth.yaml"
