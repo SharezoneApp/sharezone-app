8699f2c1ac7e0f325fc8171eea221ffb8387910a
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-02-23 15:20:56 +0100

    Update GeneratedPluginRegistrant.swift

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,9 +18,10 @@ import firebase_storage
 import flutter_native_timezone
 import package_info
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import pdfx
 import purchases_flutter
+import shared_preferences_foundation
 import shared_preferences_macos
 import sign_in_with_apple
 import sqflite
@@ -44,6 +45,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
