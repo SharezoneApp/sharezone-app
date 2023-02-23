46e6b709c809ec76ec762ae1c9d8ae55199a08b9
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-02-23 16:15:12 +0100

    Remove `package:firebase` implementations.

--- lib/remote_configuration/lib/src/get_remote_configuration.dart
+++ lib/remote_configuration/lib/src/get_remote_configuration.dart
@@ -6,12 +6,7 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import 'implementation/stub_remote_configuration.dart'
-    // ignore: uri_does_not_exist
-    if (dart.library.io) 'implementation/firebase_remote_configuration.dart'
-    // ignore: uri_does_not_exist
-    if (dart.library.js) 'implementation/firebase_web_remote_configuration.dart'
-    as implementation;
+import 'implementation/firebase_remote_configuration.dart' as implementation;
 import 'remote_configuration.dart';
 
 RemoteConfiguration getRemoteConfiguration() {
