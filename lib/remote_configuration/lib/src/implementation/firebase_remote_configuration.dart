a7fbeab019147348b2d1d89459c336dba3182f35
nilsreichardt <me@nils.re>
2023-03-07 15:08:27 +0100

    Remove `_defaultValues` from FirebaseRemoteConfiguration

--- lib/remote_configuration/lib/src/implementation/firebase_remote_configuration.dart
+++ lib/remote_configuration/lib/src/implementation/firebase_remote_configuration.dart
@@ -12,37 +12,31 @@ import 'package:sharezone_utils/platform.dart';
 import '../remote_configuration.dart';
 
 class FirebaseRemoteConfiguration extends RemoteConfiguration {
-  FirebaseRemoteConfiguration();
+  FirebaseRemoteConfiguration() {
+    _remoteConfig = FirebaseRemoteConfig.instance;
+  }
 
-  FirebaseRemoteConfig? _remoteConfig;
-  Map<String, dynamic>? _defaultValues;
+  late FirebaseRemoteConfig _remoteConfig;
 
   @override
   String getString(String key) {
-    if (_remoteConfig == null) {
-      throw RemoteConfigurationNotInitializedException();
-    }
-    return _remoteConfig!.getString(key);
+    return _remoteConfig.getString(key);
   }
 
   @override
   bool getBool(String key) {
-    if (_remoteConfig == null) {
-      throw RemoteConfigurationNotInitializedException();
-    }
-    return _remoteConfig!.getBool(key);
+    return _remoteConfig.getBool(key);
   }
 
   @override
   Future<void> initialize(Map<String, dynamic> defaultValues) async {
     try {
-      _defaultValues = defaultValues;
       _remoteConfig = FirebaseRemoteConfig.instance;
-      _remoteConfig!.setDefaults(_defaultValues!);
-      _remoteConfig!.setConfigSettings(RemoteConfigSettings(
+      _remoteConfig.setDefaults(defaultValues);
+      _remoteConfig.setConfigSettings(RemoteConfigSettings(
           fetchTimeout: const Duration(minutes: 1),
           minimumFetchInterval: const Duration(hours: 3)));
-      await _remoteConfig!.fetchAndActivate();
+      await _remoteConfig.fetchAndActivate();
     } catch (e) {
       print("Error fetch remote config: $e");
     }
