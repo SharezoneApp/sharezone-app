8a88f6ae82eaf53af5d38ba4869d3d74bf13dda7
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-03-28 15:58:06 +0200

    Migrate file_basics to null safety.

--- lib/filesharing/files_basics/lib/src/local_file/mime_type.dart
+++ lib/filesharing/files_basics/lib/src/local_file/mime_type.dart
@@ -15,7 +15,7 @@ class MimeType {
 
   static final any = MimeType('application');
 
-  factory MimeType.fromFileNameOrNull(String fileName) {
+  static MimeType? fromFileNameOrNull(String fileName) {
     final mimetype = lookupMimeType(fileName);
     if (mimetype == null || mimetype == 'null')
       return null;
@@ -23,7 +23,7 @@ class MimeType {
       return MimeType(mimetype);
   }
 
-  factory MimeType.fromPathOrNull(String path) {
+  static MimeType? fromPathOrNull(String path) {
     final mimetype = lookupMimeType(path);
     if (mimetype == null || mimetype == 'null')
       return null;
@@ -31,7 +31,7 @@ class MimeType {
       return MimeType(mimetype);
   }
 
-  factory MimeType.fromBlobType(String blobType) {
+  static MimeType? fromBlobType(String blobType) {
     final mimetype = lookupMimeType(blobType);
     if (mimetype == null || mimetype == 'null')
       return MimeType(blobType);
