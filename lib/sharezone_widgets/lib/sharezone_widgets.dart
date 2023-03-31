17c7be805457e4b5f03247f9b2c0fe2b642ba1ef
Marc Renken <marc.renken@bitbot.eu>
2023-03-31 16:58:33 +0200

    refactor(sharezone_widgets): add all exports to single file in sharezone_widgets package #538

---
+++ lib/sharezone_widgets/lib/sharezone_widgets.dart
@@ -0,0 +1,83 @@
+// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+library sharezone_widgets;
+
+// adaptive_dialog
+export 'src/adaptive_dialog/adaptive_dialog_action.dart';
+export 'src/adaptive_dialog/column_actions_dialog/column_actions_dialog.dart';
+export 'src/adaptive_dialog/left_and_right_action_dialog/left_and_right_action_adaptive_dialog.dart';
+export 'src/adaptive_dialog/long_press_dialog/long_press.dart';
+export 'src/adaptive_dialog/long_press_dialog/long_press_adaptive_dialog.dart';
+
+// additional
+export 'src/additional/gray_shimmer.dart';
+export 'src/additional/circular_button.dart';
+
+// alternating_colored_list
+export 'src/alternating_colored_list.dart';
+
+// announcement_card
+export 'src/announcement_card.dart';
+
+// cards
+export 'src/cards/overlay_card.dart';
+
+// common_widgets
+export 'src/common/models/action_item.dart';
+export 'src/common/models/simple_data.dart';
+export 'src/common/widgets/action_item_button.dart';
+
+// dialog_wrapper
+export 'src/dialog_wrapper.dart';
+
+// form
+export 'src/form.dart';
+
+// placeholder
+export 'src/placeholder.dart';
+
+// rounded_simple_dialog
+export 'src/rounded_simple_dialog.dart';
+
+// snackbars
+export 'src/snackbars.dart';
+
+// state_sheet
+export 'src/state_sheet/sheet.dart';
+export 'src/state_sheet/dialog.dart';
+export 'src/state_sheet/state_sheet_content.dart';
+export 'src/state_sheet/state_dialog_content.dart';
+export 'src/state_sheet/widgets/state_dialog_simple_body.dart';
+export 'src/state_sheet/widgets/state_sheet_simple_body.dart';
+export 'src/state_sheet/widgets/state_dialog_loading_body.dart';
+export 'src/state_sheet/widgets/state_sheet_loading_body.dart';
+
+export 'src/state_sheet/simple/simple_content_dialog.dart';
+export 'src/state_sheet/simple/simple_content_sheet.dart';
+export 'src/state_sheet/simple/simple_state_dialog.dart';
+export 'src/state_sheet/simple/simple_state_sheet.dart';
+export 'src/state_sheet/simple/appfunctions_state_dialog.dart';
+export 'common_widgets.dart';
+
+// svg
+export 'src/svg.dart';
+
+// theme
+export 'src/theme/theme.dart';
+export 'src/theme/platform.dart';
+export 'src/theme/ignore_will_pop_scope_when_ios_swipe_back_route.dart';
+export 'src/theme/scale_up_route.dart';
+export 'src/theme/fade_route.dart';
+export 'src/theme/brightness/general_theme.dart';
+
+// widgets
+export 'src/widgets.dart';
+
+// wrappable_list
+export 'src/wrappable_list.dart';
