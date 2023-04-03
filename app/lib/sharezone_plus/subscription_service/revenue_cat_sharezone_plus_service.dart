7804a11bc6a616482568828a76256b82de84b6d6
nilsreichardt <me@nils.re>
2023-04-03 17:54:27 +0200

    Add basic subscription stuff
    Co-authored-by: Jonas Sander <Jonas-Sander@users.noreply.github.com>

---
+++ app/lib/sharezone_plus/subscription_service/revenue_cat_sharezone_plus_service.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import 'package:purchases_flutter/purchases_flutter.dart';
+
+import 'purchase_service.dart';
+
+class RevenueCatPurchaseService implements PurchaseService {
+  @override
+  Future<void> purchase(ProductId id) async {
+    final offerings = await Purchases.getOfferings();
+    final availablePackages =
+        offerings.getOffering('default-donate').availablePackages;
+    final packageToPurchase = availablePackages
+        .singleWhere((package) => package.identifier == id.toString());
+    await Purchases.purchasePackage(packageToPurchase);
+  }
+
+  @override
+  Future<List<StoreProduct>> getProducts() async {
+    final offerings = await Purchases.getOfferings();
+
+    final availablePackages = offerings
+        .getOffering('default-dev-plus-subscription')
+        .availablePackages;
+    final identifiers = availablePackages
+        .map((package) => package.storeProduct.identifier)
+        .toList();
+
+    final products = await Purchases.getProducts(
+      identifiers,
+      type: PurchaseType.inapp,
+    );
+
+    return products;
+  }
+}
