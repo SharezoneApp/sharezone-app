4560717a727b80596e4c281ad2f53c326e9cf51d
nilsreichardt <me@nils.re>
2023-04-04 13:25:41 +0200

    Add subscription to user doc

---
+++ lib/user/lib/src/models/subscription.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+enum SubscriptionTier {
+  teacherPlus,
+  unknown,
+}
+
+extension EnumByNameWithDefault<T extends Enum> on Iterable<T> {
+  T tryByName(String name, {T? defaultValue}) {
+    for (T value in this) {
+      if (value.name == name) return value;
+    }
+
+    if (defaultValue != null) return defaultValue;
+    throw ArgumentError.value(name, "name", "No enum value with that name");
+  }
+}
+
+class Subscription {
+  final SubscriptionTier tier;
+  final DateTime lastSubscribedAt;
+
+  const Subscription(this.tier, this.lastSubscribedAt);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'tier': tier.name,
+      'lastSubscribedAt': lastSubscribedAt,
+    };
+  }
+
+  static Subscription? fromData(Map<String, dynamic>? map) {
+    if (map == null) return null;
+    return Subscription.fromJson(map);
+  }
+
+  factory Subscription.fromJson(Map<String, dynamic> map) {
+    return Subscription(
+      SubscriptionTier.values.tryByName(
+        map['tier'],
+        defaultValue: SubscriptionTier.unknown,
+      ),
+      DateTime.fromMillisecondsSinceEpoch(map['lastSubscribedAt']),
+    );
+  }
+
+  @override
+  String toString() =>
+      'Subscription(tier: $tier, lastSubscribedAt: $lastSubscribedAt)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Subscription &&
+        other.tier == tier &&
+        other.lastSubscribedAt == lastSubscribedAt;
+  }
+
+  @override
+  int get hashCode => Object.hash(tier, lastSubscribedAt);
+}
