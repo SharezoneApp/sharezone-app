SharezoneApp/sharezone-app

commit 9d665b93807b5c5acde4a052198031efda6fa7e1

Migrate the `user` package to null safety (#469)
Migrated to null safety. Removed `quiver` because it would require that
the `sharezone` package would upgrade to Dart 2.12 (the package was only
used for a the `hashCode` in `Period`).

Tested it manually.

Closes #182

lib/user/lib/src/models/user.dart
@@ -7,7 +7,6 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 import 'package:sharezone_common/helper_functions.dart';
-import 'package:meta/meta.dart';
 import 'features.dart';
 import 'state_enum.dart';
 import 'tips/user_tip_data.dart';
@@ -22,39 +21,39 @@ class AppUser {
 
   final TypeOfUser typeOfUser;
 
-  final String referralLink, referredBy;
+  final String? referralLink, referredBy;
   final int referralScore;
 
   final StateEnum state;
-  final String reminderTime;
-  final List<String> notificationTokens;
-  final bool blackboardNotifications;
-  final bool commentsNotifications;
+  final String? reminderTime;
+  final List<String?> notificationTokens;
+  final bool? blackboardNotifications;
+  final bool? commentsNotifications;
   final UserSettings userSettings;
   final UserTipData userTipData;
-  final DateTime createdOn;
-  final Features features;
+  final DateTime? createdOn;
+  final Features? features;
 
   AppUser._({
-    @required this.id,
-    @required this.name,
-    @required this.abbreviation,
-    @required this.typeOfUser,
-    @required this.referralLink,
-    @required this.referredBy,
-    @required this.referralScore,
-    @required this.reminderTime,
-    @required this.notificationTokens,
-    @required this.state,
-    @required this.blackboardNotifications,
-    @required this.commentsNotifications,
-    @required this.userSettings,
-    @required this.userTipData,
-    @required this.createdOn,
+    required this.id,
+    required this.name,
+    required this.abbreviation,
+    required this.typeOfUser,
+    required this.referralLink,
+    required this.referredBy,
+    required this.referralScore,
+    required this.reminderTime,
+    required this.notificationTokens,
+    required this.state,
+    required this.blackboardNotifications,
+    required this.commentsNotifications,
+    required this.userSettings,
+    required this.userTipData,
+    required this.createdOn,
     this.features,
   });
 
-  factory AppUser.create({String id}) {
+  factory AppUser.create({required String id}) {
     return AppUser._(
       id: id,
       name: "Anonymer Account",
@@ -74,7 +73,7 @@ class AppUser {
     );
   }
 
-  factory AppUser.fromData(Map<String, dynamic> data, {@required String id}) {
+  factory AppUser.fromData(Map<String, dynamic>? data, {required String id}) {
     if (data == null)
       return AppUser._(
         id: id,
@@ -97,7 +96,8 @@ class AppUser {
       id: id,
       name: data['name'],
       abbreviation: generateAbbreviation(data['name']),
-      typeOfUser: enumFromString(TypeOfUser.values, data['typeOfUser']),
+      typeOfUser: enumFromString(TypeOfUser.values, data['typeOfUser']) ??
+          TypeOfUser.unknown,
       notificationTokens: decodeList(data['notificationTokens'], (it) => it),
       reminderTime: data['reminderTime'],
       referralLink: data['referralLink'],
@@ -149,18 +149,18 @@ class AppUser {
   }
 
   AppUser copyWith({
-    String id,
-    String name,
-    String abbreviation,
-    TypeOfUser typeOfUser,
-    String reminderTime,
-    int referralScore,
-    List<String> notificationTokens,
-    StateEnum state,
-    bool blackboardNotifications,
-    bool commentsNotifications,
-    UserSettings userSettings,
-    UserTipData userTipData,
+    String? id,
+    String? name,
+    String? abbreviation,
+    TypeOfUser? typeOfUser,
+    String? reminderTime,
+    int? referralScore,
+    List<String>? notificationTokens,
+    StateEnum? state,
+    bool? blackboardNotifications,
+    bool? commentsNotifications,
+    UserSettings? userSettings,
+    UserTipData? userTipData,
   }) {
     return AppUser._(
       id: id ?? this.id,
@@ -185,7 +185,7 @@ class AppUser {
   }
 }
 
-String generateAbbreviation(String name) {
+String generateAbbreviation(String? name) {
   if (name != null) {
     if (name.length <= 1) {
       return name.toUpperCase();

app/lib/navigation/drawer/account_section.dart
@@ -21,7 +21,7 @@ class _AccountSection extends StatelessWidget {
       initialData: api.user.data,
       stream: api.user.userStream,
       builder: (context, snapshot) {
-        final user = snapshot.data ?? AppUser.create();
+        final user = snapshot.data ?? AppUser.create(id: "Id");
         return Tooltip(
           message: 'Profile',
           child: InkWell(

app/lib/overview/views/user_view.dart
@@ -55,5 +55,5 @@ class UserView {
         state = "",
         isAnonymous = false,
         provider = null,
-        user = AppUser.create();
+        user = AppUser.create(id: "");
 }

app/lib/pages/settings/my_profile/user_view.dart
@@ -45,7 +45,7 @@ class UserView {
   factory UserView.empty() {
     return UserView(
       id: "",
-      user: AppUser.create(),
+      user: AppUser.create(id: ""),
       email: "",
       name: "",
       typeOfUser: "",

app/test/account/account_page_test.dart
@@ -208,5 +208,5 @@ class MockUserGateway implements UserGateway {
   Stream<DocumentSnapshot> get userDocument => throw UnimplementedError();
 
   @override
-  Stream<AppUser> get userStream => Stream.value(AppUser.create());
+  Stream<AppUser> get userStream => Stream.value(AppUser.create(id: ""));
 }

lib/user/lib/src/models/features.dart
@@ -6,15 +6,13 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import 'package:meta/meta.dart';
-
 class Features {
   final bool allColors;
   final bool hideDonations;
 
   Features({
-    @required this.allColors,
-    @required this.hideDonations,
+    required this.allColors,
+    required this.hideDonations,
   });
 
   Map<String, dynamic> toJson() {
@@ -24,7 +22,7 @@ class Features {
     };
   }
 
-  factory Features.fromJson(Map<String, dynamic> map) {
+  factory Features.fromJson(Map<String, dynamic>? map) {
     return Features(
       allColors: map == null ? true : map['allColors'] ?? true,
       hideDonations: map == null ? false : map['hideDonations'] ?? false,
@@ -34,15 +32,16 @@ class Features {
   @override
   bool operator ==(Object o) {
     if (identical(this, o)) return true;
-  
+
     return o is Features &&
-      o.allColors == allColors &&
-      o.hideDonations == hideDonations;
+        o.allColors == allColors &&
+        o.hideDonations == hideDonations;
   }
 
   @override
   int get hashCode => allColors.hashCode ^ hideDonations.hashCode;
 
   @override
-  String toString() => 'Features(allColors: $allColors, hideDonations: $hideDonations)';
+  String toString() =>
+      'Features(allColors: $allColors, hideDonations: $hideDonations)';
 }

lib/user/lib/src/models/timetable/enabled_weekdays.dart
@@ -20,32 +20,31 @@ const weekDayDefaults = {
 };
 
 class EnabledWeekDays {
-  final Map<String, bool> _internalMap;
+  final Map<String, bool?> _internalMap;
 
   const EnabledWeekDays._(this._internalMap);
 
   static const EnabledWeekDays standard = EnabledWeekDays._({});
 
-  factory EnabledWeekDays.fromData(Map<String, dynamic> data) {
-    return EnabledWeekDays._(decodeMap<bool>(data, (key, value) => value));
+  factory EnabledWeekDays.fromData(Map<String, dynamic>? data) {
+    return EnabledWeekDays._(decodeMap<bool?>(data, (key, value) => value));
   }
 
-  bool getValue(WeekDay weekDay) {
-    return _internalMap[weekDayEnumToString(weekDay)] ??
-        weekDayDefaults[weekDay];
+  bool? getValue(WeekDay weekDay) {
+    return _internalMap[weekDay.name] ?? weekDayDefaults[weekDay];
   }
 
   EnabledWeekDays copyWith(WeekDay weekDay, bool newValue) {
     final newMap = Map.of(_internalMap);
-    newMap[weekDayEnumToString(weekDay)] = newValue;
+    newMap[weekDay.name] = newValue;
     return EnabledWeekDays._(newMap);
   }
 
   List<WeekDay> getEnabledWeekDaysList() {
-    return WeekDay.values.where((it) => getValue(it)).toList();
+    return WeekDay.values.where((it) => getValue(it)!).toList();
   }
 
-  Map<String, bool> toJson() {
+  Map<String?, bool?> toJson() {
     return _internalMap;
   }
 }

lib/user/lib/src/models/timetable/period.dart
@@ -6,11 +6,9 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+import 'package:collection/collection.dart' show IterableNullableExtension;
 import 'package:sharezone_common/helper_functions.dart';
-import 'package:meta/meta.dart';
 import 'package:time/time.dart';
-
-import 'package:quiver/core.dart';
 
 const Periods standardPeriods = Periods({
   1: Period(
@@ -52,15 +50,17 @@ const Periods standardPeriods = Periods({
 
 class Period {
   final int number;
-  final Time startTime, endTime;
+  final Time startTime;
+  final Time endTime;
 
-  const Period(
-      {@required this.number,
-      @required this.startTime,
-      @required this.endTime});
+  const Period({
+    required this.number,
+    required this.startTime,
+    required this.endTime,
+  });
 
   factory Period.fromData(
-      {@required int number, @required Map<String, dynamic> data}) {
+      {required int number, required Map<String, dynamic> data}) {
     return Period(
       number: number,
       startTime: Time.parse(data['startTime']),
@@ -82,39 +82,34 @@ class Period {
     return startTimeIsSameOrBefore && endTimeIsSameOrAfter;
   }
 
-  bool isBeforePeriod(Period previous) {
+  bool isBeforePeriod(Period? previous) {
     if (previous == null) return false;
     return startTime.totalMinutes < previous.endTime.totalMinutes;
   }
 
-  bool isAfterPeriod(Period next) {
+  bool isAfterPeriod(Period? next) {
     if (next == null) return false;
     return endTime.totalMinutes > next.startTime.totalMinutes;
   }
 
   bool validate() {
-    if (startTime == null) return false;
-    if (endTime == null) return false;
     if (startTime.totalMinutes > endTime.totalMinutes) return false;
     return true;
   }
 
   @override
-  bool operator ==(other) {
-    return other is Period &&
-        number == other.number &&
-        startTime == other.startTime &&
-        endTime == other.endTime;
-  }
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
 
-  @override
-  int get hashCode {
-    return hash3(number, startTime, endTime);
+    return other is Period &&
+        other.number == number &&
+        other.startTime == startTime &&
+        other.endTime == endTime;
   }
 
   Period copyWith({
-    Time startTime,
-    Time endTime,
+    Time? startTime,
+    Time? endTime,
   }) {
     return Period(
       number: number,
@@ -122,14 +117,17 @@ class Period {
       endTime: endTime ?? this.endTime,
     );
   }
+
+  @override
+  int get hashCode => Object.hash(number, startTime, endTime);
 }
 
 class Periods {
-  final Map<int, Period> _data;
+  final Map<int, Period?> _data;
 
   const Periods(this._data);
 
-  factory Periods.fromData(Map<String, dynamic> data) {
+  factory Periods.fromData(Map<String, dynamic>? data) {
     if (data == null) return standardPeriods;
     return Periods(
       decodeMap(
@@ -144,12 +142,12 @@ class Periods {
         MapEntry(key.toString(), value?.toJson() ?? emptyFirestoreValue()));
   }
 
-  Period getPeriod(int number) {
+  Period? getPeriod(int number) {
     return _data[number];
   }
 
   List<Period> getPeriods() {
-    return _data.values.where((period) => period != null).toList()
+    return _data.values.whereNotNull().toList()
       ..sort((p1, p2) => p1.number.compareTo(p2.number));
   }
 
@@ -163,12 +161,12 @@ class Periods {
     return Periods(newData);
   }
 
-  Period _internalCalculatePossibleNextPeriod() {
+  Period? _internalCalculatePossibleNextPeriod() {
     if (getPeriods().isEmpty) {
       return Period(
           number: 1, startTime: Time(hour: 7), endTime: Time(hour: 8));
     } else {
-      final lastPeriod = getPeriods()?.last;
+      final lastPeriod = getPeriods().last;
       final minutesOfLastPeriod =
           lastPeriod.endTime.differenceInMinutes(lastPeriod.startTime);
 
@@ -205,7 +203,7 @@ class Periods {
   }
 
   bool validate() {
-    Time lastTime;
+    Time? lastTime;
     for (final period in _data.values) {
       if (period == null) return false;
       if (!period.validate()) return false;
@@ -220,7 +218,7 @@ class Periods {
   bool isCloseToAnyPeriod(Time time) {
     for (final period in _data.values) {
       // Checks if time is in Period
-      if (period.includesTime(time)) return true;
+      if (period!.includesTime(time)) return true;
       if (period.startTime.differenceInMinutes(time) <= 30) return true;
       if (period.endTime.differenceInMinutes(time) <= 30) return true;
     }

commit f3de60475a01df4ed11467628c31bf62ba2d6d28

Fix freeze when signing out on web (#573)
Through Git binary search, I found out that the issue #497 was caused by
#469. I don't why the web app crashes when the `AppUser` is
`TypeOfUser.unknown`. We are never using the `TypeOfUser.unknown` state.
However, using `TypeOfUser.student` works and I think should be okay as
a fallback value, because `typeOfUser` should never be null.


https://user-images.githubusercontent.com/24459435/230229074-9adc6da4-e6e7-43b8-a7df-4302bbbd67d5.mov

Closes #497

lib/user/lib/src/models/user.dart
@@ -103,7 +103,7 @@ class AppUser {
       name: data['name'],
       abbreviation: generateAbbreviation(data['name']),
       typeOfUser: enumFromString(TypeOfUser.values, data['typeOfUser']) ??
-          TypeOfUser.unknown,
+          TypeOfUser.student,
       notificationTokens: decodeList(data['notificationTokens'], (it) => it),
       reminderTime: data['reminderTime'],
       referralLink: data['referralLink'],