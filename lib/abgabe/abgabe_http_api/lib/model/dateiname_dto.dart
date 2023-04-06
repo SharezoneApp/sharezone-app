5873cc658e2adab8ab98d651b68d695a7d0900d6
Nils Reichardt <me@nils.re>
2023-04-06 19:52:18 +0200

    Format `abgabe_http_api` package (#590)
    Used `dart format .` to format the package. Preparation for #588.

--- lib/abgabe/abgabe_http_api/lib/model/dateiname_dto.dart
+++ lib/abgabe/abgabe_http_api/lib/model/dateiname_dto.dart
@@ -6,22 +6,19 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-        import 'package:built_value/built_value.dart';
+import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 
 part 'dateiname_dto.g.dart';
 
-abstract class DateinameDto implements Built<DateinameDto, DateinameDtoBuilder> {
+abstract class DateinameDto
+    implements Built<DateinameDto, DateinameDtoBuilder> {
+  @BuiltValueField(wireName: r'name')
+  String get name;
 
-    
-    @BuiltValueField(wireName: r'name')
-    String get name;
+  // Boilerplate code needed to wire-up generated code
+  DateinameDto._();
 
-    // Boilerplate code needed to wire-up generated code
-    DateinameDto._();
-
-    factory DateinameDto([updates(DateinameDtoBuilder b)]) = _$DateinameDto;
-    static Serializer<DateinameDto> get serializer => _$dateinameDtoSerializer;
-
+  factory DateinameDto([updates(DateinameDtoBuilder b)]) = _$DateinameDto;
+  static Serializer<DateinameDto> get serializer => _$dateinameDtoSerializer;
 }
-
