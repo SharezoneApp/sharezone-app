fefad0698669f3d286f28377f5b6c5cbd5c1b44e
nilsreichardt <me@nils.re>
2023-05-05 16:01:48 +0200

    new link style

--- app/lib/pages/settings/support_page.dart
+++ app/lib/pages/settings/support_page.dart
@@ -173,6 +173,7 @@ class _NoteAboutPrivacyPolicy extends StatelessWidget {
       content: MarkdownBody(
         data:
             "Bitte beachte, dass bei der Nutzung von Discord dessen [Datenschutzbestimmungen](https://discord.com/privacy) gelten.",
+        styleSheet: MarkdownStyleSheet(a: linkStyle(context, 14)),
         onTapLink: (_, url, __) => launchUrl(Uri.parse(url)),
       ),
       actions: [
