5bad92a9597e7fbe78f2e61f4dd786364723383b
nilsreichardt <me@nils.re>
2023-05-25 00:05:33 +0200

    Use prod

--- app/integration_test/app_test.dart
+++ app/integration_test/app_test.dart
@@ -20,7 +20,7 @@ void main() {
   _UserCredentials user1;
 
   setUpAll(() async {
-    dependencies = await initializeDependencies(flavor: Flavor.dev);
+    dependencies = await initializeDependencies(flavor: Flavor.prod);
   });
 
   setUp(() async {
