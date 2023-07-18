5590a84b6f02fe38b2916ad7e712abb289f5ee31
Nils Reichardt <me@nils.re>
2023-07-18 10:12:08 +0200

    Add `sz build macos` build command to Sharezone Repo CLI (#689)

--- tools/sz_repo_cli/lib/src/main.dart
+++ tools/sz_repo_cli/lib/src/main.dart
@@ -14,6 +14,7 @@ import 'package:path/path.dart' as p;
 import 'package:sz_repo_cli/src/commands/src/add_license_headers_command.dart';
 import 'package:sz_repo_cli/src/commands/src/build_android_command.dart';
 import 'package:sz_repo_cli/src/commands/src/build_command.dart';
+import 'package:sz_repo_cli/src/commands/src/build_macos_command.dart';
 import 'package:sz_repo_cli/src/commands/src/build_web_command.dart';
 import 'package:sz_repo_cli/src/commands/src/build_ios_command.dart';
 import 'package:sz_repo_cli/src/commands/src/check_license_headers_command.dart';
@@ -49,6 +50,7 @@ Future<void> main(List<String> args) async {
     ..addCommand(DeployCommand()..addSubcommand(DeployWebAppCommand(repo)))
     ..addCommand(BuildCommand()
       ..addSubcommand(BuildAndroidCommand(repo))
+      ..addSubcommand(BuildMacOsCommand(repo))
       ..addSubcommand(BuildWebCommand(repo))
       ..addSubcommand(BuildIosCommand(repo)));
 
