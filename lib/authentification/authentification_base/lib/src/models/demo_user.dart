59fde3003956638f3a6dc0161c9224eecc6b50b4
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-08-09 18:54:56 +0200

    Fixed imports in demo_user.dart

--- lib/authentification/authentification_base/lib/src/models/demo_user.dart
+++ lib/authentification/authentification_base/lib/src/models/demo_user.dart
@@ -6,11 +6,9 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import 'dart:developer';
-
 import 'package:authentification_base/authentification.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_platform_interface/src/auth_provider.dart';
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
 
 class DemoUser extends AuthUser {
   DemoUser()
