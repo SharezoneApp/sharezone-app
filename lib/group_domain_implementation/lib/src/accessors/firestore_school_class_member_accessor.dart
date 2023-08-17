2574ffca8068437f15656056ced3f9d3cb202eff
nilsreichardt <me@nils.re>
2023-08-17 20:03:44 +0200

    Migrate `group_domain_implementation` to null safety

--- lib/group_domain_implementation/lib/src/accessors/firestore_school_class_member_accessor.dart
+++ lib/group_domain_implementation/lib/src/accessors/firestore_school_class_member_accessor.dart
@@ -6,9 +6,9 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:group_domain_models/group_domain_accessors.dart';
 import 'package:group_domain_models/group_domain_models.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sharezone_common/references.dart';
 
 class FirestoreSchoolClassMemberAccessor extends SchoolClassMemberAccessor {
@@ -27,8 +27,10 @@ class FirestoreSchoolClassMemberAccessor extends SchoolClassMemberAccessor {
     return _memberReference(schoolClassID).snapshots().map(
           (snapshot) => snapshot.docs
               .map(
-                (docSnap) =>
-                    MemberData.fromData(docSnap.data(), id: docSnap.id),
+                (docSnap) => MemberData.fromData(
+                  docSnap.data() as Map<String, dynamic>,
+                  id: docSnap.id,
+                ),
               )
               .toList(),
         );
@@ -37,7 +39,10 @@ class FirestoreSchoolClassMemberAccessor extends SchoolClassMemberAccessor {
   @override
   Stream<MemberData> streamSingleMember(String schoolClassID, String memberID) {
     return _memberReference(schoolClassID).doc(memberID).snapshots().map(
-          (snapshot) => MemberData.fromData(snapshot.data(), id: snapshot.id),
+          (snapshot) => MemberData.fromData(
+            snapshot.data() as Map<String, dynamic>,
+            id: snapshot.id,
+          ),
         );
   }
 }
