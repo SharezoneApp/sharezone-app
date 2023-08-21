SharezoneApp/sharezone-app

commit 50d41006e86a12e0252521f0c4ab2ad77ced85cc

Fix date are only formatted in English language (#657)
| German | English |
|--------|--------|
|
![image](https://github.com/SharezoneApp/sharezone-app/assets/24459435/cd83c894-7a1b-4d30-92bd-bc4f729d2d1b)
|
![image](https://github.com/SharezoneApp/sharezone-app/assets/24459435/ea477c3c-9e71-4f62-b424-9351661c3d7b)
|

Fixes #656

app/lib/main/plugin_initializations.dart
@@ -12,6 +12,8 @@ import 'dart:developer';
 import 'package:crash_analytics/crash_analytics.dart';
 import 'package:dynamic_links/dynamic_links.dart';
 import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/intl_standalone.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:remote_configuration/remote_configuration.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -111,6 +113,15 @@ class PluginInitializations {
     final prefs = await StreamingSharedPreferences.instance;
     return prefs;
   }
+
+  static Future<void> initializeDateFormatting() async {
+    // We need to initialize the date formatting to get the correct locale
+    // for the date formatting. Otherwise, the date formatting will be
+    // in English.
+    //
+    // Copied from https://stackoverflow.com/a/69889853/8358501.
+    Intl.systemLocale = await findSystemLocale();
+  }
 }
 
 Future<PluginInitializations> runPluginInitializations({
@@ -123,6 +134,7 @@ Future<PluginInitializations> runPluginInitializations({
       PluginInitializations.initializeStreamingSharedPreferences();
   final futureCrashAnalytics = PluginInitializations.initializeCrashAnalytics();
   final futureDynamicLinks = PluginInitializations.initializeDynamicLinks();
+  final futureDateFormatting = PluginInitializations.initializeDateFormatting();
 
   final result = await Future.wait([
     futureSharedPrefs,
@@ -130,6 +142,7 @@ Future<PluginInitializations> runPluginInitializations({
     futureStreamingSharedPrefs,
     futureCrashAnalytics,
     futureDynamicLinks,
+    futureDateFormatting,
   ]);
   return PluginInitializations(
     sharedPreferences: result[0] as SharedPreferences,

commit 0c87707a18444369b2ce42355bbfc48397602736

Fix loading date format locale on web (#774)
Caused by #657

Fixes #767

app/lib/main/plugin_initializations.dart
@@ -13,7 +13,8 @@ import 'package:crash_analytics/crash_analytics.dart';
 import 'package:dynamic_links/dynamic_links.dart';
 import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
-import 'package:intl/intl_standalone.dart';
+import 'package:intl/intl_standalone.dart'
+    if (dart.library.html) 'package:intl/intl_browser.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:remote_configuration/remote_configuration.dart';
 import 'package:shared_preferences/shared_preferences.dart';