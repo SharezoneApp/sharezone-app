SharezoneApp/sharezone-app

commit a7f3f7cda74ab5b3b427dd29f907f49ced6e33fe

Migrate `sharezone_widgets` to null safety. (#728)
Closes #185

lib/sharezone_widgets/lib/src/adaptive_dialog/column_actions_dialog/column_actions_dialog.dart
@@ -14,11 +14,11 @@ import 'package:sharezone_widgets/src/dialog_wrapper.dart';
 
 import '../adaptive_dialog_action.dart';
 
-Future<T> showColumnActionsAdaptiveDialog<T>({
-  @required BuildContext context,
-  @required List<AdaptiveDialogAction<T>> actions,
-  String title,
-  String messsage,
+Future<T?> showColumnActionsAdaptiveDialog<T>({
+  required BuildContext context,
+  required List<AdaptiveDialogAction<T>> actions,
+  String? title,
+  String? messsage,
 }) async {
   return PlatformCheck.isIOS
       ? await showCupertinoDialog<T>(
@@ -41,32 +41,32 @@ Future<T> showColumnActionsAdaptiveDialog<T>({
 
 class _ColumnActionsDialogMaterial<T> extends StatelessWidget {
   const _ColumnActionsDialogMaterial({
-    Key key,
+    Key? key,
     this.actions,
     this.title,
     this.message,
   }) : super(key: key);
 
-  final List<AdaptiveDialogAction<T>> actions;
-  final String title;
-  final String message;
+  final List<AdaptiveDialogAction<T>>? actions;
+  final String? title;
+  final String? message;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text(title),
-      content: DialogWrapper(child: Text(message)),
+      title: Text(title!),
+      content: DialogWrapper(child: Text(message!)),
       actions: <Widget>[
         Column(
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: actions
+          children: actions!
               .map((action) => TextButton(
                     style: TextButton.styleFrom(
                       foregroundColor:
                           action.textColor ?? Theme.of(context).primaryColor,
                     ),
                     onPressed: () => Navigator.pop(context, action.popResult),
-                    child: Text(action.title.toUpperCase()),
+                    child: Text(action.title!.toUpperCase()),
                   ))
               .toList(),
         )
@@ -79,25 +79,25 @@ class _ColumnActionsDialogMaterial<T> extends StatelessWidget {
 
 class _ColumnActionsDialogCupertino<T> extends StatelessWidget {
   const _ColumnActionsDialogCupertino(
-      {Key key, this.actions, this.title, this.message})
+      {Key? key, this.actions, this.title, this.message})
       : super(key: key);
 
-  final List<AdaptiveDialogAction<T>> actions;
-  final String title;
-  final String message;
+  final List<AdaptiveDialogAction<T>>? actions;
+  final String? title;
+  final String? message;
 
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      title: isNotEmptyOrNull(title) ? Text(title) : null,
-      content: isNotEmptyOrNull(message) ? Text(message) : null,
+      title: isNotEmptyOrNull(title!) ? Text(title!) : null,
+      content: isNotEmptyOrNull(message!) ? Text(message!) : null,
       actions: <Widget>[
-        ...actions
+        ...actions!
             .map((action) => CupertinoDialogAction(
                   isDefaultAction: action.isDefaultAction,
                   isDestructiveAction: action.isDestructiveAction,
                   onPressed: () => Navigator.pop(context, action.popResult),
-                  child: Text(action.title),
+                  child: Text(action.title!),
                 ))
             .toList(),
         CupertinoDialogAction(

lib/sharezone_widgets/lib/src/adaptive_dialog/left_and_right_action_dialog/left_and_right_action_adaptive_dialog.dart
@@ -12,18 +12,18 @@ import 'package:sharezone_common/helper_functions.dart';
 import 'package:sharezone_utils/platform.dart';
 import 'package:sharezone_widgets/sharezone_widgets.dart';
 
-Future<T> showLeftRightAdaptiveDialog<T>({
-  @required BuildContext context,
-  String title,
-  Widget content,
+Future<T?> showLeftRightAdaptiveDialog<T>({
+  required BuildContext? context,
+  String? title,
+  Widget? content,
   AdaptiveDialogAction left = AdaptiveDialogAction.cancle,
-  AdaptiveDialogAction right,
+  AdaptiveDialogAction? right,
   bool withCancleButtonOnIOS = false,
-  T defaultValue,
+  T? defaultValue,
 }) async {
   final result = PlatformCheck.isIOS
       ? await showCupertinoDialog<T>(
-          context: context,
+          context: context!,
           builder: (context) => _ActionAndCancleDialogCupertino(
             title: title,
             content: content,
@@ -33,7 +33,7 @@ Future<T> showLeftRightAdaptiveDialog<T>({
           ),
         )
       : await showDialog<T>(
-          context: context,
+          context: context!,
           builder: (context) => _ActionAndCancleDialogMaterial(
             title: title,
             content: content,
@@ -47,13 +47,13 @@ Future<T> showLeftRightAdaptiveDialog<T>({
 
 class LeftAndRightAdaptiveDialog<T> extends StatelessWidget {
   final AdaptiveDialogAction left;
-  final AdaptiveDialogAction right;
-  final String title;
-  final Widget content;
+  final AdaptiveDialogAction? right;
+  final String? title;
+  final Widget? content;
   final bool withCancleButtonOnIOS;
 
   const LeftAndRightAdaptiveDialog({
-    Key key,
+    Key? key,
     this.left = AdaptiveDialogAction.cancle,
     this.right,
     this.title,
@@ -75,20 +75,20 @@ class LeftAndRightAdaptiveDialog<T> extends StatelessWidget {
     return _ActionAndCancleDialogMaterial<T>(
       content: content,
       title: title,
-      left: left,
-      right: right,
+      left: left as AdaptiveDialogAction<T>?,
+      right: right as AdaptiveDialogAction<T>?,
     );
   }
 }
 
 class _ActionAndCancleDialogMaterial<T> extends StatelessWidget {
-  final AdaptiveDialogAction<T> left;
-  final AdaptiveDialogAction<T> right;
-  final String title;
-  final Widget content;
+  final AdaptiveDialogAction<T>? left;
+  final AdaptiveDialogAction<T>? right;
+  final String? title;
+  final Widget? content;
 
   const _ActionAndCancleDialogMaterial({
-    Key key,
+    Key? key,
     this.right,
     this.title,
     this.content,
@@ -98,29 +98,30 @@ class _ActionAndCancleDialogMaterial<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: isNotEmptyOrNull(title) ? Text(title) : null,
+      title: isNotEmptyOrNull(title!) ? Text(title!) : null,
       content: DialogWrapper(child: content),
       actions: <Widget>[
         if (left != null)
           TextButton(
-            key: left.key,
+            key: left!.key,
             style: TextButton.styleFrom(
-              foregroundColor: left.textColor ?? Theme.of(context).primaryColor,
+              foregroundColor:
+                  left!.textColor ?? Theme.of(context).primaryColor,
             ),
-            onPressed: left.onPressed ??
-                () => Navigator.pop(context, left.popResult ?? false),
-            child: Text(left.title.toUpperCase()),
+            onPressed: left!.onPressed ??
+                (() => Navigator.pop(context, left!.popResult ?? false)),
+            child: Text(left!.title!.toUpperCase()),
           ),
         if (right != null)
           TextButton(
-            key: right.key,
+            key: right!.key,
             style: TextButton.styleFrom(
               foregroundColor:
-                  right.textColor ?? Theme.of(context).primaryColor,
+                  right!.textColor ?? Theme.of(context).primaryColor,
             ),
-            onPressed: right.onPressed ??
-                () => Navigator.pop(context, right.popResult ?? true),
-            child: Text(right.title.toUpperCase()),
+            onPressed: right!.onPressed ??
+                (() => Navigator.pop(context, right!.popResult ?? true)),
+            child: Text(right!.title!.toUpperCase()),
           )
       ],
     );
@@ -128,14 +129,14 @@ class _ActionAndCancleDialogMaterial<T> extends StatelessWidget {
 }
 
 class _ActionAndCancleDialogCupertino extends StatelessWidget {
-  final AdaptiveDialogAction right;
-  final AdaptiveDialogAction left;
-  final String title;
-  final Widget content;
-  final bool withCancleButtonOnIOS;
+  final AdaptiveDialogAction? right;
+  final AdaptiveDialogAction? left;
+  final String? title;
+  final Widget? content;
+  final bool? withCancleButtonOnIOS;
 
   const _ActionAndCancleDialogCupertino({
-    Key key,
+    Key? key,
     this.right,
     this.title,
     this.content,
@@ -146,28 +147,28 @@ class _ActionAndCancleDialogCupertino extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      title: isNotEmptyOrNull(title) ? Text(title) : const Text(""),
+      title: isNotEmptyOrNull(title!) ? Text(title!) : const Text(""),
       content: content,
       actions: <Widget>[
         if (left != null)
           CupertinoDialogAction(
-            key: left.key,
-            isDefaultAction: left.isDefaultAction,
-            isDestructiveAction: left.isDestructiveAction,
-            onPressed:
-                left.onPressed ?? () => Navigator.pop(context, left.popResult),
-            child: Text(left.title),
+            key: left!.key,
+            isDefaultAction: left!.isDefaultAction,
+            isDestructiveAction: left!.isDestructiveAction,
+            onPressed: left!.onPressed ??
+                (() => Navigator.pop(context, left!.popResult)),
+            child: Text(left!.title!),
           ),
         if (right != null)
           CupertinoDialogAction(
-            key: right.key,
-            isDefaultAction: right.isDefaultAction,
-            isDestructiveAction: right.isDestructiveAction,
-            onPressed: right.onPressed ??
-                () => Navigator.pop(context, right.popResult),
-            child: Text(right.title),
+            key: right!.key,
+            isDefaultAction: right!.isDefaultAction,
+            isDestructiveAction: right!.isDestructiveAction,
+            onPressed: right!.onPressed ??
+                (() => Navigator.pop(context, right!.popResult)),
+            child: Text(right!.title!),
           ),
-        if (ThemePlatform.isCupertino && withCancleButtonOnIOS)
+        if (ThemePlatform.isCupertino && withCancleButtonOnIOS!)
           CupertinoDialogAction(
             onPressed: () => Navigator.pop(context),
             isDestructiveAction: true,

lib/sharezone_widgets/lib/src/adaptive_dialog/long_press_dialog/long_press_adaptive_dialog.dart
@@ -12,13 +12,13 @@ import 'package:sharezone_common/helper_functions.dart';
 import 'package:sharezone_utils/platform.dart';
 import 'package:sharezone_widgets/sharezone_widgets.dart';
 
-Future<T> showLongPressAdaptiveDialog<T>({
-  @required BuildContext context,
-  @required List<LongPress<T>> longPressList,
-  String title,
-  String subtile,
+Future<T?> showLongPressAdaptiveDialog<T>({
+  required BuildContext context,
+  required List<LongPress<T>> longPressList,
+  String? title,
+  String? subtile,
 }) async {
-  return PlatformCheck.isIOS
+  return (PlatformCheck.isIOS
       ? await showCupertinoModalPopup<T>(
           context: context,
           builder: (context) => _LongPressDialogCupertino<T>(
@@ -31,11 +31,11 @@ Future<T> showLongPressAdaptiveDialog<T>({
           context: context,
           builder: (context) =>
               _LongPressDialogMaterial<T>(longPressList: longPressList),
-        );
+        )) as Future<T?>;
 }
 
 class _LongPressDialogMaterial<T> extends StatelessWidget {
-  const _LongPressDialogMaterial({Key key, @required this.longPressList})
+  const _LongPressDialogMaterial({Key? key, required this.longPressList})
       : super(key: key);
 
   final List<LongPress<T>> longPressList;
@@ -60,17 +60,17 @@ class _LongPressDialogMaterial<T> extends StatelessWidget {
 
 class _LongPressDialogCupertino<T> extends StatelessWidget {
   const _LongPressDialogCupertino(
-      {Key key, this.title, this.subtitle, @required this.longPressList})
+      {Key? key, this.title, this.subtitle, required this.longPressList})
       : super(key: key);
 
-  final String title, subtitle;
+  final String? title, subtitle;
   final List<LongPress<T>> longPressList;
 
   @override
   Widget build(BuildContext context) {
     return CupertinoActionSheet(
-      title: isNotEmptyOrNull(title) ? Text(title) : null,
-      message: isNotEmptyOrNull(subtitle) ? Text(subtitle) : null,
+      title: isNotEmptyOrNull(title!) ? Text(title!) : null,
+      message: isNotEmptyOrNull(subtitle!) ? Text(subtitle!) : null,
       actions: longPressList
           .map((longPress) => CupertinoActionSheetAction(
               child: Text(longPress.title),

lib/sharezone_widgets/lib/src/placeholder.dart
@@ -16,21 +16,21 @@ import 'package:sharezone_widgets/sharezone_widgets.dart';
 /// Hier wird definiert, wie das Widget aufgebaut sein soll
 class PlaceholderModel extends StatefulWidget {
   const PlaceholderModel({
-    Key key,
+    Key? key,
     this.title,
     this.subtitle,
     this.svgPath,
     this.iconSize,
-    @required this.animateSVG,
+    required this.animateSVG,
     this.rivePath,
     this.riveAnimationName,
   }) : super(key: key);
 
-  final String title, svgPath, rivePath;
-  final Widget subtitle;
-  final Size iconSize;
+  final String? title, svgPath, rivePath;
+  final Widget? subtitle;
+  final Size? iconSize;
   final bool animateSVG;
-  final String riveAnimationName;
+  final String? riveAnimationName;
 
   @override
   PlaceholderModelState createState() => PlaceholderModelState();
@@ -38,7 +38,7 @@ class PlaceholderModel extends StatefulWidget {
 
 class PlaceholderModelState extends State<PlaceholderModel>
     with TickerProviderStateMixin {
-  AnimationController _controllerIcon;
+  AnimationController? _controllerIcon;
 
   @override
   void initState() {
@@ -48,7 +48,7 @@ class PlaceholderModelState extends State<PlaceholderModel>
       vsync: this,
     );
 
-    if (widget.animateSVG) _controllerIcon.repeat();
+    if (widget.animateSVG) _controllerIcon!.repeat();
   }
 
   @override
@@ -72,7 +72,7 @@ class PlaceholderModelState extends State<PlaceholderModel>
             Padding(
               padding: const EdgeInsets.only(top: 12),
               child: _RotateAnimation(
-                controller: _controllerIcon,
+                controller: _controllerIcon!,
                 size: widget.iconSize,
                 path: widget.svgPath,
                 rivePath: widget.rivePath,
@@ -80,12 +80,12 @@ class PlaceholderModelState extends State<PlaceholderModel>
               ),
             ),
             const SizedBox(height: 7.5),
-            if (isNotEmptyOrNull(widget.title))
+            if (isNotEmptyOrNull(widget.title!))
               Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Text(
-                  widget.title,
+                  widget.title!,
                   style: const TextStyle(fontSize: 20),
                   textAlign: TextAlign.center,
                 ),
@@ -111,10 +111,10 @@ class PlaceholderModelState extends State<PlaceholderModel>
 
 class PlaceholderWidgetWithAnimation extends StatelessWidget {
   const PlaceholderWidgetWithAnimation({
-    Key key,
-    @required this.title,
+    Key? key,
+    required this.title,
     this.description,
-    @required this.svgPath,
+    required this.svgPath,
     this.iconSize = const Size(175, 175),
     this.animateSVG = false,
     this.scrollable = true,
@@ -122,7 +122,7 @@ class PlaceholderWidgetWithAnimation extends StatelessWidget {
   }) : super(key: key);
 
   final String title, svgPath;
-  final Widget description;
+  final Widget? description;
   final Size iconSize;
   final bool animateSVG, scrollable, center;
 
@@ -133,7 +133,7 @@ class PlaceholderWidgetWithAnimation extends StatelessWidget {
       svgPath: svgPath,
       title: title,
       subtitle: description,
-      iconSize: iconSize ?? const Size(175, 175),
+      iconSize: iconSize,
     );
 
     if (!center && !scrollable) return w;
@@ -145,8 +145,8 @@ class PlaceholderWidgetWithAnimation extends StatelessWidget {
 
 class _RotateAnimation extends StatelessWidget {
   _RotateAnimation({
-    Key key,
-    this.controller,
+    Key? key,
+    required this.controller,
     this.size,
     this.path,
     this.rivePath,
@@ -169,16 +169,16 @@ class _RotateAnimation extends StatelessWidget {
   final Animation<double> controller;
   final Animation<double> rotate;
 
-  final Size size;
-  final String path;
-  final String rivePath;
-  final String riveAnimationName;
+  final Size? size;
+  final String? path;
+  final String? rivePath;
+  final String? riveAnimationName;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       animation: controller,
-      builder: (BuildContext context, Widget child) {
+      builder: (BuildContext context, Widget? child) {
         return Container(
           alignment: Alignment.center,
           child: RotationTransition(
@@ -202,21 +202,21 @@ class _RotateAnimation extends StatelessWidget {
 
 class _Rive extends StatelessWidget {
   const _Rive({
-    Key key,
-    @required this.size,
-    @required this.path,
-    @required this.animationName,
+    Key? key,
+    required this.size,
+    required this.path,
+    required this.animationName,
   }) : super(key: key);
 
-  final Size size;
-  final String path;
-  final String animationName;
+  final Size? size;
+  final String? path;
+  final String? animationName;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: size.height,
-      width: size.width,
+      height: size!.height,
+      width: size!.width,
       child: FlareActor(
         path,
         animation: animationName,
@@ -229,13 +229,13 @@ class _Rive extends StatelessWidget {
 /// Ghost SVG
 class SVGIcon extends StatefulWidget {
   const SVGIcon({
-    Key key,
+    Key? key,
     this.size,
-    @required this.path,
+    required this.path,
   }) : super(key: key);
 
-  final Size size;
-  final String path;
+  final Size? size;
+  final String? path;
 
   @override
   SVGIconState createState() => SVGIconState();
@@ -245,9 +245,9 @@ class SVGIconState extends State<SVGIcon> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: widget.size.width,
-      height: widget.size.height,
-      child: PlatformSvg.asset(widget.path),
+      width: widget.size!.width,
+      height: widget.size!.height,
+      child: PlatformSvg.asset(widget.path!),
     );
   }
 }

lib/sharezone_common/lib/src/helper_functions.dart
@@ -51,7 +51,7 @@ T? enumFromString<T>(List<T?> values, dynamic json, {T? orElse}) => json != null
 String? enumToString<T>(T value) =>
     value != null ? value.toString().split('.')[1] : null;
 
-bool isNotEmptyOrNull(String value) => !isEmptyOrNull(value);
+bool isNotEmptyOrNull(String? value) => !isEmptyOrNull(value);
 
 bool isEmptyOrNull(String? value) {
   return value == null || value.isEmpty;

lib/sharezone_widgets/lib/src/adaptive_dialog/adaptive_dialog_action.dart
@@ -9,11 +9,11 @@
 import 'package:flutter/material.dart';
 
 class AdaptiveDialogAction<T> {
-  final Key key;
-  final String title;
-  final Color textColor;
-  final T popResult;
-  final VoidCallback onPressed;
+  final Key? key;
+  final String? title;
+  final Color? textColor;
+  final T? popResult;
+  final VoidCallback? onPressed;
 
   /// Set to true if button is the default choice in the dialog.
   ///

lib/sharezone_widgets/lib/src/adaptive_dialog/long_press_dialog/long_press.dart
@@ -11,11 +11,11 @@ import 'package:flutter/material.dart';
 class LongPress<T> {
   final String title;
   final T popResult;
-  final Widget icon;
+  final Widget? icon;
 
   const LongPress({
-    @required this.title,
-    @required this.popResult,
+    required this.title,
+    required this.popResult,
     this.icon,
   });
 }

commit b679be7f4d5e0b995ad4322f28f5ca2adad2b3df

Fix broken null safety migration of `sharezone_widgets` (#788)
Bug introduced in #728.

Fixes #787

lib/sharezone_widgets/lib/src/adaptive_dialog/column_actions_dialog/column_actions_dialog.dart
@@ -89,8 +89,8 @@ class _ColumnActionsDialogCupertino<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      title: isNotEmptyOrNull(title!) ? Text(title!) : null,
-      content: isNotEmptyOrNull(message!) ? Text(message!) : null,
+      title: isNotEmptyOrNull(title) ? Text(title!) : null,
+      content: isNotEmptyOrNull(message) ? Text(message!) : null,
       actions: <Widget>[
         ...actions!
             .map((action) => CupertinoDialogAction(

lib/sharezone_widgets/lib/src/adaptive_dialog/left_and_right_action_dialog/left_and_right_action_adaptive_dialog.dart
@@ -98,7 +98,7 @@ class _ActionAndCancleDialogMaterial<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: isNotEmptyOrNull(title!) ? Text(title!) : null,
+      title: isNotEmptyOrNull(title) ? Text(title!) : null,
       content: DialogWrapper(child: content),
       actions: <Widget>[
         if (left != null)
@@ -147,7 +147,7 @@ class _ActionAndCancleDialogCupertino extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      title: isNotEmptyOrNull(title!) ? Text(title!) : const Text(""),
+      title: isNotEmptyOrNull(title) ? Text(title!) : const Text(""),
       content: content,
       actions: <Widget>[
         if (left != null)

lib/sharezone_widgets/lib/src/adaptive_dialog/long_press_dialog/long_press_adaptive_dialog.dart
@@ -69,8 +69,8 @@ class _LongPressDialogCupertino<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoActionSheet(
-      title: isNotEmptyOrNull(title!) ? Text(title!) : null,
-      message: isNotEmptyOrNull(subtitle!) ? Text(subtitle!) : null,
+      title: isNotEmptyOrNull(title) ? Text(title!) : null,
+      message: isNotEmptyOrNull(subtitle) ? Text(subtitle!) : null,
       actions: longPressList
           .map((longPress) => CupertinoActionSheetAction(
               child: Text(longPress.title),

lib/sharezone_widgets/lib/src/placeholder.dart
@@ -80,7 +80,7 @@ class PlaceholderModelState extends State<PlaceholderModel>
               ),
             ),
             const SizedBox(height: 7.5),
-            if (isNotEmptyOrNull(widget.title!))
+            if (isNotEmptyOrNull(widget.title))
               Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 8, horizontal: 16),

lib/sharezone_widgets/lib/src/widgets.dart
@@ -916,7 +916,7 @@ class CustomCardListTile extends StatelessWidget {
                     style: Theme.of(context).textTheme.headlineSmall,
                     textAlign: TextAlign.center,
                   ),
-                  if (isNotEmptyOrNull(subtitle!)) ...[
+                  if (isNotEmptyOrNull(subtitle)) ...[
                     const SizedBox(height: 2),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 16),