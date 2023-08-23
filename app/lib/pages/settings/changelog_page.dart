a944583af4ea48aeacf1797e06ac18c83432f400
Nils Reichardt <me@nils.re>
2023-08-23 10:07:13 +0200

    Improve `UpdatePromptCard` (#782)
    * Do not show "download new version from Play Store" on the web anymore
    * Add golden tests
    * Use "https://sharezone.net/mobile-store" instead of
    "https://onelink.to"
    * Write "App Store" and "Play Store" instead of "AppStore" and
    "PlayStore"
    * Translate comment to English

--- app/lib/pages/settings/changelog_page.dart
+++ app/lib/pages/settings/changelog_page.dart
@@ -168,33 +168,39 @@ class _ChangeParagraph extends StatelessWidget {
 }
 
 class UpdatePromptCard extends StatelessWidget {
+  String _getText() {
+    if (PlatformCheck.isWeb) {
+      return "Wir haben bemerkt, dass du eine veraltete Version der App verwendest. Lade die Seite neu, um die neuste Version zu erhalten! 👍";
+    }
+
+    return "Wir haben bemerkt, dass du eine veraltete Version der App installiert hast. Lade dir deswegen jetzt die Version im ${PlatformCheck.isMacOsOrIOS ? "App Store" : "Play Store"} herunter! 👍";
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnnouncementCard(
       key: ValueKey('UpdatePromptCard'),
-      onTap: () => launchURL(getStoreLink()),
+      onTap: PlatformCheck.isWeb
+          ? null
+          : () => launchURL(getStoreLink(), context: context),
       title: "Neues Update verfügbar!",
       padding: const EdgeInsets.all(0),
       titleColor: Colors.white,
-      color: const Color(0xFFF44336),
+      color: Colors.orange,
       content: Text(
-        "Wir haben bemerkt, dass du eine veraltete Version der App installiert hast. Lade dir deswegen unbedingt jetzt die Version im ${ThemePlatform.isCupertino ? "AppStore" : "PlayStore"} herunter! 👍",
+        _getText(),
         style: const TextStyle(color: Colors.white),
       ),
     );
   }
 
   String getStoreLink() {
-    // Öffnet direkt den Mac AppStore (funktioniert nur auf macOS-Geräten)
+    // Opens the Mac AppStore directly (only works on macOS devices)
     if (PlatformCheck.isMacOS) return 'https://sharezone.net/macos-direct';
 
-    // Es sollte für iOS & Android NICHT https://sharezone.net/ios oder
-    // https://sharezone.net/android verwendet werden, weil der Nutzer direkt
-    // wieder in die App geleitet wird, wenn diese installiert ist. Der Nutzer
-    // wird nur in den Store geleitet, wenn dieser die App nicht installiert hat.
-    //
-    // Der "onelink.to" leitet immer in den Store - egal, ob die App installiert
-    // ist oder nicht.
-    return "http://onelink.to/eqkmz5";
+    if (PlatformCheck.isAndroid) return 'https://sharezone.net/android';
+    if (PlatformCheck.isIOS) return 'https://sharezone.net/ios';
+
+    throw Exception('Unsupported platform');
   }
 }
---
+++ app/test_goldens/pages/settings/changelog_page_test.dart
@@ -0,0 +1,50 @@
+// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:sharezone/pages/settings/changelog_page.dart';
+import 'package:sharezone_utils/platform.dart';
+
+void main() {
+  group(UpdatePromptCard, () {
+    for (final platform in [
+      Platform.android,
+      Platform.iOS,
+      Platform.web,
+      Platform.macOS
+    ]) {
+      testGoldens('display card for ${platform.name} as expected',
+          (tester) async {
+        // ignore: invalid_use_of_visible_for_testing_member
+        PlatformCheck.setCurrentPlatformForTesting(platform);
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    UpdatePromptCard(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+
+        await screenMatchesGolden(
+          tester,
+          'update_prompt_card_${platform.name.toLowerCase()}',
+        );
+      });
+    }
+  });
+}
