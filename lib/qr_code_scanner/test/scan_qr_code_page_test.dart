01f691012984722744b1ca787bc9c3d9565f4c08
nilsreichardt <me@nils.re>
2023-08-23 12:16:59 +0200

    Remove unused import

--- lib/qr_code_scanner/test/scan_qr_code_page_test.dart
+++ lib/qr_code_scanner/test/scan_qr_code_page_test.dart
@@ -8,7 +8,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import 'fake_mobile_scanner_controller.dart';
