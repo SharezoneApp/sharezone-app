SharezoneApp/sharezone-app

commit 3688ce525289ff75979a0953e1675d73bf091ac9

Replace `sign_in_with_apple` and `firebase_auth_oauth` with `firebase_auth` (#776)
## Description

* Using the built-in social auth methods (removing `sign_in_with_apple`
and `firebase_auth_oauth`)
* Replace the `firebaseAuthUser.reload()` strategy with `userChanges()`
stream
* Back then, `userChanges()` wasn't available what is the reason why we
used `.reload()` after linking the Sharezone account with a sign in
method
* Add support for Google Sign In on web in dev environment
* Users with Google Sign In doesn't pass their email address anymore
(more GDPR friendly)


![image](https://github.com/SharezoneApp/sharezone-app/assets/24459435/5557c5a4-8213-44af-b828-7c5daa82a53e)


## Testing

Manually testing, if the following features are working:

* Sign In
* Link account with Apple/Google
* Delete account (need to reauthenticate)

| Provider | Android | iOS | macOS | Web |
|--------|--------|--------|--------|--------|
| Google Sign In | Works | Works | Works | Works |
| Apple Sign In | Works | Works | Works | Works |

## Related Tickets

Fixes #650
Fixes #649

app/lib/util/api/user_api.dart
@@ -35,18 +35,10 @@ class UserGateway implements UserGatewayAuthentifcation {
 
   AppUser get data => _userSubject.valueOrNull;
 
-  // Auf Grund eines Fehlers im FirebaseAuth Plugin, wird bei der
-  // reload() Methode nicht der firebaseUser sofort neu geladen.
-  // So muss man diesen mit reload() neuladen und danach wieder
-  // neu mit FirebaseAuth.instance.currentUser den neuen User ziehen.
-  // Dieser neue Nutzer muss dann mit alten User überschrieben werden.
-  // Damit dies möglich ist, darf der User nicht final sein.
-  // Ticket: https://github.com/flutter/flutter/issues/20390
-  AuthUser authUser;
-
   // Firebase User Stream
   final _authUserSubject = BehaviorSubject<AuthUser>();
   Stream<AuthUser> get authUserStream => _authUserSubject;
+  AuthUser get authUser => _authUserSubject.value;
 
   Future<AppUser> get() {
     return references.users
@@ -63,14 +55,18 @@ class UserGateway implements UserGatewayAuthentifcation {
   Stream<Provider> get providerStream =>
       _authUserSubject.map((user) => _getProvider(user.firebaseUser));
 
-  UserGateway(this.references, this.authUser) : uID = authUser.uid {
+  UserGateway(this.references, AuthUser authUser) : uID = authUser.uid {
     _appUserSubscription =
         references.users.doc(uID).snapshots().map((documentSnapshot) {
       return AppUser.fromData(documentSnapshot.data(), id: uID);
     }).listen((newUser) {
       _userSubject.sink.add(newUser);
     });
+
     _authUserSubject.sink.add(authUser);
+    _authUserSubject.sink.addStream(
+      FirebaseAuth.instance.userChanges().map(AuthUser.fromFirebaseUser),
+    );
   }
 
   Future<void> logOut() async {
@@ -87,21 +83,9 @@ class UserGateway implements UserGatewayAuthentifcation {
 
   String getEmail() => authUser.email;
   bool isAnonymous() => authUser.isAnonymous;
+
   Stream<bool> isAnonymousStream() =>
       _authUserSubject.map((authUser) => authUser.isAnonymous);
-
-  /// Manually refreshes the data of the current user (for example,
-  /// attached providers, display name, and so on).
-  @override
-  Future<void> reloadFirebaseUser() async {
-    authUser.firebaseUser.reload();
-
-    // Auf Grund Nutzer eines Fehlers im FirebaseAuth Plugin muss
-    // nochmal neu geladen und gesetzt werden.
-    // Ticket: https://github.com/flutter/flutter/issues/20390
-    authUser = AuthUser.fromFirebaseUser(FirebaseAuth.instance.currentUser);
-    _authUserSubject.sink.add(authUser);
-  }
 
   Provider _getProvider(User fbUser) {
     if (fbUser == null || fbUser.isAnonymous) {
@@ -127,40 +111,40 @@ class UserGateway implements UserGatewayAuthentifcation {
   }
 
   Future<void> changeState(StateEnum state) async {
-    await references.users.doc(authUser.uid).update({"state": state.index});
+    await references.users.doc(uID).update({"state": state.index});
   }
 
   Future<void> addNotificationToken(String token) async {
-    await references.users.doc(authUser.uid).update({
+    await references.users.doc(uID).update({
       "notificationTokens": FieldValue.arrayUnion([token])
     });
   }
 
   void removeNotificationToken(String token) {
-    references.users.doc(authUser.uid).update({
+    references.users.doc(uID).update({
       "notificationTokens": FieldValue.arrayRemove([token])
     });
   }
 
   Future<void> setHomeworkReminderTime(TimeOfDay timeOfDay) async {
-    await references.users.doc(authUser.uid).update(
+    await references.users.doc(uID).update(
         {"reminderTime": timeOfDay?.toApiString() ?? FieldValue.delete()});
   }
 
   Future<void> updateSettings(UserSettings userSettings) async {
     await references.users
-        .doc(authUser.uid)
+        .doc(uID)
         .set({"settings": userSettings.toJson()}, SetOptions(merge: true));
   }
 
   Future<void> updateSettingsSingleFiled(String fieldName, dynamic data) async {
-    await references.users.doc(authUser.uid).set({
+    await references.users.doc(uID).set({
       "settings": {fieldName: data},
     }, SetOptions(merge: true));
   }
 
   Future<void> updateUserTip(UserTipKey userTipKey, bool value) async {
-    await references.users.doc(authUser.uid).set({
+    await references.users.doc(uID).set({
       "tips": {
         userTipKey.key: value,
       },
@@ -168,15 +152,11 @@ class UserGateway implements UserGatewayAuthentifcation {
   }
 
   void setBlackboardNotifications(bool enabled) {
-    references.users
-        .doc(authUser.uid)
-        .update({"blackboardNotifications": enabled});
+    references.users.doc(uID).update({"blackboardNotifications": enabled});
   }
 
   void setCommentsNotifications(bool enabled) {
-    references.users
-        .doc(authUser.uid)
-        .update({"commentsNotifications": enabled});
+    references.users.doc(uID).update({"commentsNotifications": enabled});
   }
 
   Future<void> changeEmail(String email) async {
@@ -187,7 +167,7 @@ class UserGateway implements UserGatewayAuthentifcation {
   Future<void> addUser({@required AppUser user, bool merge = false}) async {
     assert(user != null);
     await references.users
-        .doc(authUser.uid)
+        .doc(uID)
         .set(user.toCreateJson(), SetOptions(merge: true));
 
     return;

app/ios/Runner.xcodeproj/project.pbxproj
@@ -384,7 +384,6 @@
 				"${BUILT_PRODUCTS_DIR}/purchases_flutter/purchases_flutter.framework",
 				"${BUILT_PRODUCTS_DIR}/share/share.framework",
 				"${BUILT_PRODUCTS_DIR}/shared_preferences_foundation/shared_preferences_foundation.framework",
-				"${BUILT_PRODUCTS_DIR}/sign_in_with_apple/sign_in_with_apple.framework",
 				"${BUILT_PRODUCTS_DIR}/sqflite/sqflite.framework",
 				"${BUILT_PRODUCTS_DIR}/url_launcher_ios/url_launcher_ios.framework",
 				"${BUILT_PRODUCTS_DIR}/video_player_avfoundation/video_player_avfoundation.framework",
@@ -447,7 +446,6 @@
 				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/purchases_flutter.framework",
 				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/share.framework",
 				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/shared_preferences_foundation.framework",
-				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/sign_in_with_apple.framework",
 				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/sqflite.framework",
 				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/url_launcher_ios.framework",
 				"${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/video_player_avfoundation.framework",

app/lib/account/register_account_section.dart
@@ -8,7 +8,6 @@
 
 import 'package:analytics/analytics.dart';
 import 'package:authentification_base/authentification_analytics.dart';
-import 'package:authentification_base/authentification_apple.dart';
 import 'package:bloc_provider/bloc_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -82,41 +81,25 @@ class _SignInMethods extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return FutureBuilder<bool>(
-      future: AppleSignInLogic.isSignInGetCredentailsAvailable(),
-      builder: (context, snapshot) {
-        final isAppleSignInAvailable = snapshot.data ?? false;
-        if (isAppleSignInAvailable) {
-          if (width < 550)
-            return Column(
-              children: <Widget>[
-                _AppleButton.long(),
-                const SizedBox(height: 8),
-                _GoogleButton.long(),
-                const SizedBox(height: 8),
-                _EmailButton.long(),
-              ],
-            );
+    if (width < 550)
+      return Column(
+        children: <Widget>[
+          _AppleButton.long(),
+          const SizedBox(height: 8),
+          _GoogleButton.long(),
+          const SizedBox(height: 8),
+          _EmailButton.long(),
+        ],
+      );
 
-          return Row(
-            children: <Widget>[
-              Expanded(child: _EmailButton.short()),
-              const SizedBox(width: 8),
-              Expanded(child: _AppleButton.short()),
-              const SizedBox(width: 8),
-              Expanded(child: _GoogleButton.short()),
-            ],
-          );
-        }
-
-        return Row(
-          children: <Widget>[
-            Expanded(child: _EmailButton.short()),
-            const SizedBox(width: 8),
-            Expanded(child: _GoogleButton.short()),
-          ],
-        );
-      },
+    return Row(
+      children: <Widget>[
+        Expanded(child: _EmailButton.short()),
+        const SizedBox(width: 8),
+        Expanded(child: _AppleButton.short()),
+        const SizedBox(width: 8),
+        Expanded(child: _GoogleButton.short()),
+      ],
     );
   }
 }

app/lib/pages/settings/my_profile/my_profile_page.dart
@@ -433,19 +433,15 @@ class _DeleteAccountDialogContentState
 
     try {
       if (provider != Provider.anonymous) {
-        AuthCredential credential;
         if (provider == Provider.google) {
           final googleSignInLogic = GoogleSignInLogic();
-          final googleSignInCredential = await googleSignInLogic.signIn();
-          credential = googleSignInCredential;
+          await googleSignInLogic.reauthenticateWithGoogle();
         } else if (provider == Provider.apple) {
           final appleSignInLogic = AppleSignInLogic();
-          await appleSignInLogic.signIn();
+          await appleSignInLogic.reauthenticateWithApple();
         } else {
-          credential = EmailAuthProvider.credential(
+          final credential = EmailAuthProvider.credential(
               email: fbUser.email, password: password);
-        }
-        if (credential != null) {
           await fbUser.reauthenticateWithCredential(credential);
         }
       }

app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -24,7 +24,6 @@ import path_provider_foundation
 import pdfx
 import purchases_flutter
 import shared_preferences_foundation
-import sign_in_with_apple
 import sqflite
 import url_launcher_macos
 import wakelock_macos
@@ -49,7 +48,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))

app/test/account/account_page_test.dart
@@ -163,11 +163,6 @@ class MockUserGateway implements UserGateway {
   References get references => throw UnimplementedError();
 
   @override
-  Future<void> reloadFirebaseUser() {
-    throw UnimplementedError();
-  }
-
-  @override
   void removeNotificationToken(String token) {}
 
   @override

app/web/index.html
@@ -64,10 +64,6 @@
     code to be more GDPR friendly (client avoid making a request to a third
     party service) -->
     <script src="https://unpkg.com/@zxing/library@0.19.1" type="application/javascript"></script>
-    
-    <!-- Required for Google Sign In -->
-    <meta name="google-signin-client_id"
-        content="730263787697-s5tqfln3apv1j27netnnv8p8305ns3j9.apps.googleusercontent.com">
 
     <script>
         // The value below is injected by flutter build, do not touch.

lib/authentification/authentification_base/lib/src/analytics/login_analytics.dart
@@ -21,7 +21,7 @@ class LoginAnalytics {
 
   LoginAnalytics(this._analytics);
 
-  void logGoolgeLogin() {
+  void logGoogleLogin() {
     _analytics.log(AuthentifactionEvent(provider: google, name: name));
   }
 

commit a79919f4e93f1d6043b4908e3022ee1136085092

Fix linking account with email & password on iOS & macOS (#817)
## Description

The issue for #813 was that the `isAnonymous` attribute of the Firebase
user has not been updated when linking an anonymous account with email
address & password on iOS and macOS. The workaround is to use `reload()`
on the Firebase auth user. The bug was caused by #776.

## Working Demo


https://github.com/SharezoneApp/sharezone-app/assets/24459435/c3646063-1179-4584-a789-025f12636c5b

## Related Tickets

Fixes #813

app/lib/util/api/user_api.dart
@@ -107,6 +107,14 @@ class UserGateway implements UserGatewayAuthentifcation {
   Future<User> linkWithCredential(AuthCredential credential) async {
     final authResult =
         await authUser.firebaseUser.linkWithCredential(credential);
+
+    // Even when we are using the `userChanges()` stream, we still need to call
+    // `reload()` because the `isAnonymous` property is not updated on iOS (and
+    // macOS). When the bug is fixed, we can remove this call.
+    //
+    // Bug report: https://github.com/firebase/flutterfire/issues/11520
+    await authUser.firebaseUser.reload();
+
     return authResult.user;
   }
 