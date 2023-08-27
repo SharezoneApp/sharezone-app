456cd4dc15a5576240de3899fb7ab547b1a9f8d8
nilsreichardt <me@nils.re>
2023-08-27 21:10:30 +0200

    Migrate "app/lib/navigation/scaffold/portable/bottom_navigation_bar/navigation_experiment/navigation_experiment_cache.dart"

--- app/lib/navigation/scaffold/portable/bottom_navigation_bar/navigation_experiment/navigation_experiment_cache.dart
+++ app/lib/navigation/scaffold/portable/bottom_navigation_bar/navigation_experiment/navigation_experiment_cache.dart
@@ -6,10 +6,11 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:bloc_base/bloc_base.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:sharezone/util/cache/streaming_key_value_store.dart';
-import 'package:sharezone_common/helper_functions.dart';
 
 import 'navigation_experiment_option.dart';
 
@@ -27,20 +28,13 @@ class NavigationExperimentCache extends BlocBase {
   NavigationExperimentCache(this._cache) {
     _cache
         .getString(key,
-            defaultValue: enumToString(NavigationExperimentOption.drawerAndBnb))
-        .map(_toEnum)
+            defaultValue: NavigationExperimentOption.drawerAndBnb.name)
+        .map(NavigationExperimentOption.values.byName)
         .listen(_currentNavigationSubject.sink.add);
   }
 
-  NavigationExperimentOption _toEnum(String stringValue) {
-    return enumFromString<NavigationExperimentOption>(
-      NavigationExperimentOption.values,
-      stringValue,
-    );
-  }
-
   void setNavigation(NavigationExperimentOption option) {
-    _cache.setString(key, enumToString(option));
+    _cache.setString(key, option.name);
   }
 
   @override
