b801b32fc44899bab309bdc9e7eee8e7578da650
nilsreichardt <me@nils.re>
2023-08-27 20:55:17 +0200

    Migrate "app/lib/pages/settings/src/subpages/about/widgets/team.dart"

--- app/lib/pages/settings/src/subpages/about/widgets/team.dart
+++ app/lib/pages/settings/src/subpages/about/widgets/team.dart
@@ -6,6 +6,8 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:flutter/material.dart';
 import 'package:sharezone_common/helper_functions.dart';
 import 'package:sharezone_widgets/sharezone_widgets.dart';
