703baba1173fe45d4981124ea3ac89b52c2725b6
nilsreichardt <me@nils.re>
2023-08-27 18:32:20 +0200

    Migrate "app/lib/blackboard/blocs/blackboard_card_bloc.dart"

--- app/lib/blackboard/blocs/blackboard_card_bloc.dart
+++ app/lib/blackboard/blocs/blackboard_card_bloc.dart
@@ -6,15 +6,19 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:bloc_base/bloc_base.dart';
-import 'package:meta/meta.dart';
 import 'package:sharezone/util/api/blackboard_api.dart';
 
 class BlackboardCardBloc extends BlocBase {
   final BlackboardGateway gateway;
   final String itemID;
 
-  BlackboardCardBloc({@required this.gateway, @required this.itemID});
+  BlackboardCardBloc({
+    required this.gateway,
+    required this.itemID,
+  });
 
   void changeReadState(bool newState) =>
       gateway.changeIsBlackboardDoneTo(itemID, newState);
