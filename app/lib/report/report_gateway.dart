bb81d2088ea394d83ea6e9d71cdfde787afdbe5f
nilsreichardt <me@nils.re>
2023-08-27 21:35:41 +0200

    Migrate "app/lib/report/report_gateway.dart"

--- app/lib/report/report_gateway.dart
+++ app/lib/report/report_gateway.dart
@@ -6,6 +6,8 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:bloc_base/bloc_base.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sharezone/report/report.dart';
