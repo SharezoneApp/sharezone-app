a67c16d48a10a63081826c35adf985c8d121a8c5
nilsreichardt <me@nils.re>
2023-08-28 01:33:30 +0200

    Migrate "app/lib/filesharing/bloc/file_page_bloc.dart"

--- app/lib/filesharing/bloc/file_page_bloc.dart
+++ app/lib/filesharing/bloc/file_page_bloc.dart
@@ -6,26 +6,28 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'dart:developer';
 
 import 'package:bloc_base/bloc_base.dart';
 import 'package:files_basics/local_file.dart';
 import 'package:files_usecases/file_downloader.dart';
-import 'package:meta/meta.dart';
 import 'package:rxdart/subjects.dart';
 
 class FilePageBloc extends BlocBase {
   final _localFileSubject = BehaviorSubject<LocalFile>();
 
-  FilePageBloc(
-      {@required String downloadURL,
-      @required String name,
-      @required String id}) {
-    getFileDownloader()
+  FilePageBloc({
+    required String downloadURL,
+    required String name,
+    required String id,
+  }) {
+    getFileDownloader()!
         .downloadFileFromURL(downloadURL, name, id)
         .then((localFile) {
       _localFileSubject.add(localFile);
-    }).catchError((e, StackTrace s) {
+    }).catchError((Object e, StackTrace s) {
       log('$e', error: e, stackTrace: s);
       _localFileSubject.addError(e);
     });
