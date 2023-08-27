49715667081481ccf969a1dc59bfe4127f7281b9
nilsreichardt <me@nils.re>
2023-08-27 20:48:04 +0200

    Migrate  "app/lib/navigation/scaffold/app_bar_configuration.dart"

--- app/lib/navigation/scaffold/app_bar_configuration.dart
+++ app/lib/navigation/scaffold/app_bar_configuration.dart
@@ -6,15 +6,17 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:flutter/material.dart';
 import 'package:sharezone/navigation/models/navigation_item.dart';
 
 class AppBarConfiguration {
   /// Default is [navigationItem.getName()]
-  final String title;
+  final String? title;
 
   final List<Widget> actions;
-  final PreferredSizeWidget bottom;
+  final PreferredSizeWidget? bottom;
   final double elevation;
 
   const AppBarConfiguration({
@@ -30,14 +32,15 @@ class SliverAppBarConfiguration {
   final Widget title;
 
   final List<Widget> actions;
-  final PreferredSizeWidget flexibleSpace;
-  final double expandedHeight, elevation;
-  final bool pinned;
-  final Color backgroundColor;
-  final Color drawerIconColor;
+  final PreferredSizeWidget? flexibleSpace;
+  final double? expandedHeight;
+  final double elevation;
+  final bool? pinned;
+  final Color? backgroundColor;
+  final Color? drawerIconColor;
 
   const SliverAppBarConfiguration({
-    @required this.actions,
+    required this.actions,
     this.title = const _AppBarTitle(),
     this.flexibleSpace,
     this.expandedHeight,
@@ -49,14 +52,14 @@ class SliverAppBarConfiguration {
 }
 
 class _AppBarTitle extends StatelessWidget {
-  const _AppBarTitle({Key key}) : super(key: key);
+  const _AppBarTitle({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       NavigationItem.overview.getName(),
       style:
-          Theme.of(context).textTheme.titleLarge.copyWith(color: Colors.white),
+          Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.white),
       key: const ValueKey('dashboard-appbar-title-E2E'),
     );
   }
