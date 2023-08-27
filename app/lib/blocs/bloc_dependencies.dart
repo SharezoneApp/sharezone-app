9053c9e724b864f9a811274c82e75c84b315e7b4
nilsreichardt <me@nils.re>
2023-08-28 01:36:06 +0200

    Migrate "app/lib/blocs/bloc_dependencies.dart"

--- app/lib/blocs/bloc_dependencies.dart
+++ app/lib/blocs/bloc_dependencies.dart
@@ -6,6 +6,8 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:analytics/analytics.dart';
 import 'package:app_functions/app_functions.dart';
 import 'package:authentification_base/authentification.dart';
@@ -13,7 +15,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:key_value_store/key_value_store.dart';
-import 'package:meta/meta.dart';
 import 'package:remote_configuration/remote_configuration.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sharezone_common/references.dart';
@@ -32,20 +33,20 @@ class BlocDependencies {
   final AppFunctions appFunctions;
   final Analytics analytics;
 
-  AuthUser authUser;
+  AuthUser? authUser;
 
   BlocDependencies({
-    @required this.sharedPreferences,
+    required this.sharedPreferences,
     this.authUser,
-    @required this.keyValueStore,
-    @required this.references,
-    @required this.auth,
-    @required this.analytics,
-    @required this.firestore,
-    @required this.streamingSharedPreferences,
-    @required this.registrationGateway,
-    @required this.appFunctions,
-    @required this.functions,
-    this.remoteConfiguration,
+    required this.keyValueStore,
+    required this.references,
+    required this.auth,
+    required this.analytics,
+    required this.firestore,
+    required this.streamingSharedPreferences,
+    required this.registrationGateway,
+    required this.appFunctions,
+    required this.functions,
+    required this.remoteConfiguration,
   });
 }
