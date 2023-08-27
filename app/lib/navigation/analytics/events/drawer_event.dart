8af4c9f693041f8324db0aad5760e62c5cdd1ddd
nilsreichardt <me@nils.re>
2023-08-27 20:49:45 +0200

    Migrate "app/lib/navigation/analytics/events/drawer_event.dart"

--- app/lib/navigation/analytics/events/drawer_event.dart
+++ app/lib/navigation/analytics/events/drawer_event.dart
@@ -6,12 +6,10 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import 'package:sharezone_common/helper_functions.dart';
+//@dart=2.12
 
 import 'navigation_event.dart';
 
 class DrawerEvent extends NavigationEvent {
-  DrawerEvent(String name)
-      : assert(isNotEmptyOrNull(name)),
-        super("drawer_$name");
+  DrawerEvent(String name) : super("drawer_$name");
 }
