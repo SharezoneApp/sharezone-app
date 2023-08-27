c6f4f9d1f6d88b5f29dba1ad560dc96e8f385462
nilsreichardt <me@nils.re>
2023-08-27 20:51:35 +0200

    Migrate "app/lib/util/platform_information_manager/flutter_platform_information_retreiver.dart"

--- app/lib/util/platform_information_manager/flutter_platform_information_retreiver.dart
+++ app/lib/util/platform_information_manager/flutter_platform_information_retreiver.dart
@@ -6,11 +6,13 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+//@dart=2.12
+
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:sharezone/util/platform_information_manager/platform_information_retreiver.dart';
 
 class FlutterPlatformInformationRetriever extends PlatformInformationRetriever {
-  PackageInfo _packageInfo;
+  late PackageInfo _packageInfo;
 
   @override
   Future<void> init() async {
@@ -19,30 +21,21 @@ class FlutterPlatformInformationRetriever extends PlatformInformationRetriever {
 
   @override
   String get appName {
-    assertPackageInfoIsNotNull();
     return _packageInfo.appName;
   }
 
   @override
   String get packageName {
-    assertPackageInfoIsNotNull();
     return _packageInfo.packageName;
   }
 
   @override
   String get version {
-    assertPackageInfoIsNotNull();
     return _packageInfo.version;
   }
 
   @override
   String get versionNumber {
-    assertPackageInfoIsNotNull();
     return _packageInfo.buildNumber;
-  }
-
-  void assertPackageInfoIsNotNull() {
-    assert(_packageInfo != null,
-        "PackageInfo should not be null. init() needs to be called before attributes can be read");
   }
 }
