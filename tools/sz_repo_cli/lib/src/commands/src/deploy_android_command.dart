994e0ecfe303561258cbdad53213b70be1ea1990
nilsreichardt <me@nils.re>
2023-09-04 17:48:12 +0200

    Set up deploy android command

--- tools/sz_repo_cli/lib/src/commands/src/deploy_android_command.dart
+++ tools/sz_repo_cli/lib/src/commands/src/deploy_android_command.dart
@@ -6,15 +6,17 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 import 'package:sz_repo_cli/src/common/common.dart';
 
-final _iosStages = [
+final _androidStages = [
   'stable',
-  'alpha',
+  'beta',
 ];
 
-/// The different flavors of the Anroid app that support deployment.
+/// The different flavors of the Android app that support deployment.
 final _androidFlavors = [
   'prod',
 ];
@@ -27,22 +29,16 @@ class DeployAndroidCommand extends Command {
       ..addOption(
         releaseStageOptionName,
         abbr: 's',
-        allowed: _iosStages,
+        allowed: _androidStages,
         help:
-            'The stage to deploy to. The "stable" stage is used for Play Store '
-            'releases, the "alpha" stage is used for Firebase Distribtion '
-            'releases.',
+            'The stage to deploy to. The "stable" and "beta" stage is used for '
+            'Play Store releases.',
         defaultsTo: 'stable',
-      )
-      ..addOption(
-        googleApplicationCredentialsOptionName,
-        help:
-            'Google Cloud service account credentials with `JSON` key type to access Google Play Developer API. If not given, the value will be checked from the environment variable `GCLOUD_SERVICE_ACCOUNT_CREDENTIALS`.',
       )
       ..addOption(
         whatsNewOptionName,
         help:
-            "Release notes either for the Play Store or Firebase Distribtion submission. Describe what's new in this version of your app, such as new features, improvements, and bug fixes. The string should not exceed 500 characters when you publish the Play Store. Example usage: --whats-new 'Bug fixes and performance improvements.'",
+            "Release notes either for the Play Store submission. Describe what's new in this version of your app, such as new features, improvements, and bug fixes. The string should not exceed 500 characters when you publish the Play Store. Example usage: --whats-new 'Bug fixes and performance improvements.'",
       )
       ..addOption(
         flavorOptionName,
@@ -52,14 +48,18 @@ class DeployAndroidCommand extends Command {
       );
   }
 
-  static const googleApplicationCredentialsOptionName = 'credentials';
   static const releaseStageOptionName = 'stage';
   static const flavorOptionName = 'flavor';
   static const whatsNewOptionName = 'whats-new';
 
+  static const _changelogDirectory = 'fastlane/metadata/android/de-DE';
+  static const _defaultChangelogFileName = 'default.txt';
+  static const _changelogFilePath =
+      '$_changelogDirectory/$_defaultChangelogFileName';
+
   @override
   String get description =>
-      'Deploys the Sharezone Android app to the Play Store or Firebase Distribiton. Automatically bumps the build number and builds the app. Codemagic CLI tools & Fastlane are required.';
+      'Deploys the Sharezone Android app to the Play Store. Automatically bumps the build number and builds the app. Codemagic CLI tools & Fastlane are required.';
 
   @override
   String get name => 'android';
@@ -67,6 +67,7 @@ class DeployAndroidCommand extends Command {
   @override
   Future<void> run() async {
     _throwIfFlavorIsNotSupportForDeployment();
+    _checkIfGooglePlayCredentialsAreValid();
 
     // Is used so that runProcess commands print the command that was run. Right
     // now this can't be done via an argument.
@@ -78,7 +79,7 @@ class DeployAndroidCommand extends Command {
     await _buildApp(buildNumber: buildNumber);
     await _publish();
 
-    print('Deployment finished 🎉 ');
+    stdout.writeln('Deployment finished 🎉 ');
   }
 
   void _throwIfFlavorIsNotSupportForDeployment() {
@@ -90,22 +91,20 @@ class DeployAndroidCommand extends Command {
     }
   }
 
-  Future<void> _publish() async {
-    final whatsNew = argResults![whatsNewOptionName] as String?;
-    final stage = argResults![releaseStageOptionName] as String;
-
-    final isStable = stage == 'stable';
-    await runProcess('fastlane', ['deploy'],
-        workingDirectory: '${_repo.sharezoneFlutterApp.location.path}/android',
-        environment: {
-          'WHAT_S_NEW': whatsNew ?? '',
-        });
+  /// Checks if Fastlane can establish a connection to Google Play.
+  ///
+  /// See https://docs.fastlane.tools/actions/validate_play_store_json_key
+  Future<void> _checkIfGooglePlayCredentialsAreValid() async {
+    await runProcessSucessfullyOrThrow(
+      'fastlane',
+      ['run', 'validate_play_store_json_key'],
+    );
   }
 
   Future<int> _getNextBuildNumber() async {
     final latestBuildNumber = await _getLatestBuildNumberFromGooglePlay();
     final nextBuildNumber = latestBuildNumber + 1;
-    print('Next build number: $nextBuildNumber');
+    stdout.writeln('Next build number: $nextBuildNumber');
     return nextBuildNumber;
   }
 
@@ -138,7 +137,7 @@ class DeployAndroidCommand extends Command {
           'run',
           'sz_repo_cli',
           'build',
-          'ios',
+          'android',
           '--flavor',
           flavor,
           '--stage',
@@ -149,7 +148,68 @@ class DeployAndroidCommand extends Command {
         workingDirectory: _repo.sharezoneCiCdTool.path,
       );
     } catch (e) {
-      throw Exception('Failed to build iOS app: $e');
+      throw Exception('Failed to build Android app: $e');
+    }
+  }
+
+  Future<void> _publish() async {
+    try {
+      await _setChangelog();
+
+      final track = _getGooglePlayTrackFromStage();
+      await _uploadToGooglePlay(track: track);
+    } finally {
+      await _removeChangelogFile();
+    }
+  }
+
+  Future<void> _setChangelog() async {
+    final whatsNew = argResults![whatsNewOptionName] as String?;
+    if (whatsNew == null) {
+      stdout.writeln('No changelog given. Skipping.');
+      return;
+    }
+
+    final changelogFile = File(_changelogFilePath);
+
+    // Create folder, if it doesn't exist.
+    await changelogFile.parent.create(recursive: true);
+
+    // Write changelog into file. Fastlane will pick it up automatically.
+    //
+    // See: https://docs.fastlane.tools/actions/upload_to_play_store/#changelogs-whats-new
+    await changelogFile.writeAsString(whatsNew);
+  }
+
+  String _getGooglePlayTrackFromStage() {
+    final stage = argResults![releaseStageOptionName] as String;
+    switch (stage) {
+      case 'stable':
+        return 'production';
+      case 'beta':
+        return 'beta';
+      default:
+        throw Exception('Unknown stage: $stage');
+    }
+  }
+
+  Future<void> _uploadToGooglePlay({
+    required String track,
+  }) async {
+    await runProcess(
+      'fastlane',
+      ['deploy'],
+      workingDirectory: '${_repo.sharezoneFlutterApp.location.path}/android',
+      environment: {
+        'TRACK': track,
+      },
+    );
+  }
+
+  Future<void> _removeChangelogFile() async {
+    final changelogFile = File(_changelogFilePath);
+    if (await changelogFile.exists()) {
+      await changelogFile.delete();
     }
   }
 }
