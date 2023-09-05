a3f6fc8e461bb4c90f93cb3446b7cac6d61fe42a
nilsreichardt <me@nils.re>
2023-09-05 17:01:27 +0200

    Add advantages

--- app/lib/sharezone_plus/sharezone_plus_page.dart
+++ app/lib/sharezone_plus/sharezone_plus_page.dart
@@ -28,37 +28,44 @@ class SharezonePlusPage extends StatelessWidget {
     return SharezoneMainScaffold(
       navigationItem: NavigationItem.sharezonePlus,
       body: _PageTheme(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              children: [
-                _Header(),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: SingleChildScrollView(
+            child: MaxWidthConstraintBox(
+              maxWidth: 750,
+              child: Column(
+                children: [
+                  _Header(),
+                  const SizedBox(height: 18),
+                  _WhyPlusSharezoneCard(),
+                  const SizedBox(height: 18),
+                  _Advantages(),
 
-                /// Gets plus product and shows name and price and subscription payment interval
-                FutureBuilder<StoreProduct>(
-                  future: purchaseService.getPlusSubscriptionProduct(),
-                  builder: (context, snapshot) => ListTile(
-                    leading: subscriptionService.isSubscriptionActive()
-                        ? Icon(Icons.check)
-                        : Icon(Icons.close),
-                    title: Text('Sharezone Plus'),
-                    trailing: Text(snapshot.data?.priceString ?? '...'),
+                  /// Gets plus product and shows name and price and subscription payment interval
+                  FutureBuilder<StoreProduct>(
+                    future: purchaseService.getPlusSubscriptionProduct(),
+                    builder: (context, snapshot) => ListTile(
+                      leading: subscriptionService.isSubscriptionActive()
+                          ? Icon(Icons.check)
+                          : Icon(Icons.close),
+                      title: Text('Sharezone Plus'),
+                      trailing: Text(snapshot.data?.priceString ?? '...'),
+                    ),
                   ),
-                ),
-                ElevatedButton(
-                  child: Text('Abonnieren'),
-                  onPressed: () async {
-                    final purchaseService = RevenueCatPurchaseService();
-                    final products = await purchaseService.getProducts();
-                    print(products);
+                  ElevatedButton(
+                    child: Text('Abonnieren'),
+                    onPressed: () async {
+                      final purchaseService = RevenueCatPurchaseService();
+                      final products = await purchaseService.getProducts();
+                      print(products);
 
-                    await purchaseService
-                        .purchase(ProductId('default-dev-plus-subscription'));
-                  },
-                ),
-                _FaqSection(),
-              ],
+                      await purchaseService
+                          .purchase(ProductId('default-dev-plus-subscription'));
+                    },
+                  ),
+                  _FaqSection(),
+                ],
+              ),
             ),
           ),
         ),
@@ -81,7 +88,10 @@ class _PageTheme extends StatelessWidget {
       data: baseTheme.copyWith(
         textTheme: baseTheme.textTheme.copyWith(
           bodyMedium: TextStyle(
-            color: Colors.grey,
+            color: Colors.grey[600],
+          ),
+          headlineSmall: baseTheme.textTheme.headlineSmall?.copyWith(
+            fontSize: 18,
           ),
         ),
       ),
@@ -122,15 +132,164 @@ class _Header extends StatelessWidget {
   }
 }
 
+class _WhyPlusSharezoneCard extends StatelessWidget {
+  const _WhyPlusSharezoneCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaxWidthConstraintBox(
+      maxWidth: 710,
+      child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(17.5),
+            color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+          ),
+          child: Column(
+            children: const [
+              _WhyPlusSharezoneImage(),
+              Padding(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    _WhyPlusSharezoneHeadline(),
+                    SizedBox(height: 8),
+                    _WhyPlusSharezoneText(),
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+class _WhyPlusSharezoneText extends StatelessWidget {
+  const _WhyPlusSharezoneText();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Neben einem Studium können wir, Jonas und Nils, die monatlichen Kosten von 1.000 € für Sharezone nicht selbst tragen. Daher haben wir Sharezone Plus entwickelt - der Schlüssel zur Fortführung und Werbefreiheit unserer App.',
+      style: Theme.of(context).textTheme.bodyMedium,
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _WhyPlusSharezoneHeadline extends StatelessWidget {
+  const _WhyPlusSharezoneHeadline();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Warum kostet Sharezone Plus Geld?',
+      style: Theme.of(context).textTheme.headlineSmall,
+    );
+  }
+}
+
+class _WhyPlusSharezoneImage extends StatelessWidget {
+  const _WhyPlusSharezoneImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: 200,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(17.5),
+        child: Image.asset(
+          'assets/images/jonas-nils.png',
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+      ),
+    );
+  }
+}
+
+class _Advantages extends StatelessWidget {
+  const _Advantages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _AdvantageTile(
+          icon: Icon(Icons.favorite),
+          title: Text('Untersützung von Open-Source'),
+          description: Text(
+              'Sharezone ist Open-Source. Das bedeutet, dass jeder den Quellcode von Sharezone einsehen und sogar verbessern kann. Wir glauben, dass Open-Source die Zukunft ist und wollen Sharezone zu einem Vorzeigeprojekt machen.'),
+        ),
+      ],
+    );
+  }
+}
+
+class _AdvantageTile extends StatelessWidget {
+  const _AdvantageTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.subtitle,
+    required this.description,
+  });
+
+  final Icon icon;
+  final Widget title;
+  final Widget? subtitle;
+  final Widget description;
+
+  @override
+  Widget build(BuildContext context) {
+    const green = Color(0xFF6FCF97);
+    return ExpansionCard(
+      header: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: green.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconTheme(
+                data: IconThemeData(color: green),
+                child: icon,
+              ),
+            ),
+          ),
+          const SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              title,
+              if (subtitle != null) subtitle!,
+            ],
+          ),
+        ],
+      ),
+      body: description,
+      backgroundColor: Colors.transparent,
+    );
+  }
+}
+
 class _FaqSection extends StatelessWidget {
   const _FaqSection();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        _WhoIsBehindSharezone(),
-      ],
+    return MaxWidthConstraintBox(
+      maxWidth: 710,
+      child: Column(
+        children: const [
+          _WhoIsBehindSharezone(),
+        ],
+      ),
     );
   }
 }
