b581e979dec7b8ee8db7a754c490a88f0859205e
nilsreichardt <me@nils.re>
2023-09-05 17:34:58 +0200

    Add `SharezonePlusPageBloc`

---
+++ app/lib/sharezone_plus/page/sharezone_plus_page_bloc.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:bloc_base/bloc_base.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:sharezone/sharezone_plus/page/sharezone_plus_page_view.dart';
+import 'package:sharezone/sharezone_plus/subscription_service/purchase_service.dart';
+import 'package:sharezone/sharezone_plus/subscription_service/revenue_cat_sharezone_plus_service.dart';
+
+class SharezonePlusPageBloc extends BlocBase {
+  final BehaviorSubject<SharezonePlusPageView> view =
+      BehaviorSubject<SharezonePlusPageView>.seeded(
+          SharezonePlusPageView.empty());
+
+  late StreamSubscription<bool> _hasPlusSubscription;
+  late RevenueCatPurchaseService _purchaseService;
+
+  SharezonePlusPageBloc({
+    required Stream<bool> hasPlus,
+    required RevenueCatPurchaseService purchaseService,
+  }) {
+    _purchaseService = purchaseService;
+    _listenToPlusStatus(hasPlus);
+    _getPlusPrice(purchaseService);
+  }
+
+  Future<void> _getPlusPrice(RevenueCatPurchaseService purchaseService) async {
+    final product = await purchaseService.getPlusSubscriptionProduct();
+    view.add(view.value.copyWith(price: product.priceString));
+  }
+
+  void _listenToPlusStatus(Stream<bool> hasPlus) {
+    _hasPlusSubscription = hasPlus.listen((hasPlus) {
+      view.add(view.value.copyWith(hasPlus: hasPlus));
+    });
+  }
+
+  Future<void> buy() async {
+    //TODO: Implement
+
+    final purchaseService = RevenueCatPurchaseService();
+    final products = await purchaseService.getProducts();
+    print(products);
+
+    await purchaseService.purchase(ProductId('default-dev-plus-subscription'));
+  }
+
+  @override
+  void dispose() {
+    view.close();
+    _hasPlusSubscription.cancel();
+  }
+}
