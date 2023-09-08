2474ed4811ce53ba5c692e47203388fca60ce352
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-09-08 22:42:07 +0200

    Add missing license headers.

--- app/lib/sharezone_plus/sharezone_plus_fallback_price.dart
+++ app/lib/sharezone_plus/sharezone_plus_fallback_price.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 /// Used as a hardcoded fallback price for Sharezone Plus if the price cannot be
 /// fetched via any other method.
 const fallbackSharezonePlusPriceWithCurrencySymbol = '4,99 €';
