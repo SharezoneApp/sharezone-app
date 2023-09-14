3b65a6a46bf68c0c1a891f677f6b2e6255996012
Nils Reichardt <me@nils.re>
2023-09-14 15:53:50 +0200

    Add `SharezonePlusFeatureInfoCard` widget (#955)
    ![image](https://github.com/SharezoneApp/sharezone-app/assets/24459435/90f98f2e-54d7-4175-9a90-5cea7f9c9ffd)
    
    This card is typically used when the user tries to access a Sharezone
    Plus feature without having Sharezone Plus.

--- lib/sharezone_widgets/lib/src/sharezone_plus/sharezone_plus_widgets.dart
+++ lib/sharezone_widgets/lib/src/sharezone_plus/sharezone_plus_widgets.dart
@@ -71,3 +71,105 @@ class SharezonePlusCard extends StatelessWidget {
     );
   }
 }
+
+/// A card that displays the [SharezonePlusBadge] at the top center and a "Learn
+/// more" button at the bottom center.
+///
+/// This card is typically used when the user tries to access a Sharezone Plus
+/// feature without having Sharezone Plus.
+class SharezonePlusFeatureInfoCard extends StatelessWidget {
+  const SharezonePlusFeatureInfoCard({
+    super.key,
+    required this.child,
+    this.withSharezonePlusBadge = true,
+    this.withLearnMoreButton = false,
+    this.onLearnMorePressed,
+    this.maxWidth = 400,
+  }) : assert(withLearnMoreButton == false || onLearnMorePressed != null);
+
+  /// Whether the card should display the [SharezonePlusBadge] at the top
+  /// center.
+  final bool withSharezonePlusBadge;
+
+  /// Weather the card should display the "Learn more" button at the bottom
+  /// center.
+  final bool withLearnMoreButton;
+
+  /// The callback that is called when the "Learn more" button is pressed.
+  ///
+  /// Typically, this should navigate to the Sharezone Plus page. We can't
+  /// provide a default implementation, because our current navigation
+  /// implementation is in the app package.
+  ///
+  /// If [withLearnMoreButton] is `true`, this must not be `null`.
+  final VoidCallback? onLearnMorePressed;
+
+  /// The widget that is displayed below the [SharezonePlusBadge] and above the
+  /// "Learn more" button.
+  final Widget child;
+
+  /// The maximum width of the card.
+  final double maxWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    final fontColor = Theme.of(context).isDarkTheme
+        ? Theme.of(context).primaryColor
+        : darkPrimaryColor;
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: maxWidth,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(12.5),
+        ),
+        child: DefaultTextStyle.merge(
+          style: TextStyle(
+            color: fontColor,
+          ),
+          textAlign: TextAlign.center,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (withSharezonePlusBadge)
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(8, 4, 10, 4),
+                    child: SharezonePlusBadge(),
+                  ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: child,
+                ),
+                if (withLearnMoreButton)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 4, 10, 4),
+                    child: TextButton(
+                      onPressed: onLearnMorePressed,
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        foregroundColor: fontColor,
+                      ),
+                      child: const Text(
+                        'MEHR ERFAHREN',
+                        style: TextStyle(
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
