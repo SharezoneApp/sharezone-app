8a63f3df81cad3c272ed02406123ba87cab9a2f2
Nils Reichardt <me@nils.re>
2023-09-19 18:17:19 +0200

    Add golden tests for `showSharezonePlusFeatureInfoDialog` (#1022)

---
+++ lib/sharezone_widgets/test_goldens/sharezone_plus/sharezone_plus_feature_info_dialog_test.dart
@@ -0,0 +1,69 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:sharezone_widgets/sharezone_widgets.dart';
+
+void main() {
+  group('showSharezonePlusFeatureInfoDialog', () {
+    Future<void> pumpScaffoldWithButtonForDialog(
+      WidgetTester tester, {
+      ThemeData? theme,
+    }) async {
+      await tester.pumpWidgetBuilder(
+        Scaffold(
+          body: Builder(
+            builder: (context) => Center(
+              child: ElevatedButton(
+                onPressed: () => showSharezonePlusFeatureInfoDialog(
+                  context: context,
+                  navigateToPlusPage: () {},
+                  description: const Text(
+                      'With Sharezone Plus you receive this awesome feature that changes your life!'),
+                  title: const Text('Awesome feature'),
+                ),
+                child: const Text('SHOW DIALOG'),
+              ),
+            ),
+          ),
+        ),
+        wrapper: materialAppWrapper(theme: theme),
+      );
+    }
+
+    Future<void> openDialog(WidgetTester tester) async {
+      await tester.tap(find.text('SHOW DIALOG'));
+      await tester.pumpAndSettle();
+    }
+
+    testGoldens('renders as expected (light mode)', (tester) async {
+      await pumpScaffoldWithButtonForDialog(tester, theme: ThemeData.light());
+
+      await openDialog(tester);
+
+      await screenMatchesGolden(
+          tester, 'sharezone_plus_feature_info_dialog_light');
+    });
+
+    testGoldens('renders as expected (dark mode)', (tester) async {
+      await pumpScaffoldWithButtonForDialog(
+        tester,
+        theme: ThemeData.dark().copyWith(
+          primaryColor: primaryColor,
+        ),
+      );
+
+      await openDialog(tester);
+
+      await screenMatchesGolden(
+          tester, 'sharezone_plus_feature_info_dialog_dark');
+    });
+  });
+}
