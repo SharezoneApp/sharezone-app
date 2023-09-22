ce30274b66784165cbaca1fc574156947d0ddfe4
nilsreichardt <me@nils.re>
2023-09-22 22:45:12 +0200

    Add lh

--- app/lib/calendrical_events/page/past_calendrical_events_page.dart
+++ app/lib/calendrical_events/page/past_calendrical_events_page.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sharezone/calendrical_events/provider/past_calendrical_events_page_controller.dart';
