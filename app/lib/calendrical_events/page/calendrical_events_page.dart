b7ac84aca63e93447704497275aeb2eb84d209d1
nilsreichardt <me@nils.re>
2023-09-23 01:12:39 +0200

    Do not show past events button until sharezone plus is not unlocked

--- app/lib/calendrical_events/page/calendrical_events_page.dart
+++ app/lib/calendrical_events/page/calendrical_events_page.dart
@@ -8,6 +8,7 @@
 
 import 'package:bloc_provider/bloc_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sharezone/calendrical_events/bloc/calendrical_events_page_bloc.dart';
 import 'package:sharezone/calendrical_events/bloc/calendrical_events_page_bloc_factory.dart';
 import 'package:sharezone/calendrical_events/page/past_calendrical_events_page.dart';
@@ -16,6 +17,7 @@ import 'package:sharezone/navigation/logic/navigation_bloc.dart';
 import 'package:sharezone/navigation/models/navigation_item.dart';
 import 'package:sharezone/navigation/scaffold/app_bar_configuration.dart';
 import 'package:sharezone/navigation/scaffold/sharezone_main_scaffold.dart';
+import 'package:sharezone/sharezone_plus/subscription_service/subscription_flag.dart';
 import 'package:sharezone/timetable/src/widgets/events/calender_event_card.dart';
 import 'package:sharezone/timetable/src/widgets/events/event_view.dart';
 import 'package:sharezone/timetable/timetable_page/timetable_page.dart';
@@ -74,6 +76,13 @@ class _PastEventsIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isSharezonePlusEnabled =
+        context.watch<SubscriptionEnabledFlag>().isEnabled;
+
+    if (!isSharezonePlusEnabled) {
+      return const SizedBox();
+    }
+
     return IconButton(
       tooltip: 'Vergangene Termine',
       onPressed: () {
