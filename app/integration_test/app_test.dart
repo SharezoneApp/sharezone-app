113a5eec319b4216cef8c18ab0c0e21fa5bd7355
nilsreichardt <me@nils.re>
2023-09-24 14:31:39 +0200

    Ensure that the user is loaded in the integration tests

--- app/integration_test/app_test.dart
+++ app/integration_test/app_test.dart
@@ -75,6 +75,12 @@ void main() {
       await tester.tap(find.byKey(const Key('login-button-E2E')));
       await tester.pumpAndSettle();
 
+      // Ensure that the user document is loaded. Otherwise, the user might see
+      // for short moment the page to select the type of user which could fail
+      // the test.
+      await tester
+          .pumpUntil(find.byKey(const Key('dashboard-appbar-title-E2E')));
+
       expect(
         find.byKey(const Key('dashboard-appbar-title-E2E')),
         findsOneWidget,
