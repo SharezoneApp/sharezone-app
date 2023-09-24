072876d626a4ba95722d67235bb47edc05d17f86
nilsreichardt <me@nils.re>
2023-09-24 20:34:57 +0200

    Add license header

--- website/lib/legal/imprint.dart
+++ website/lib/legal/imprint.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 class Imprint {
   /// Impressum wird als Markdown-String übergeben. Beispiel:
   ///
--- website/lib/legal/imprint_page.dart
+++ website/lib/legal/imprint_page.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:sharezone_website/page.dart';
