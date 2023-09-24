6a3f7eb7d92a02c330e76c5ed850e69def513311
nilsreichardt <me@nils.re>
2023-09-24 18:06:01 +0200

    Add lh

--- app/test/settings/support/support_page_controller_test.dart
+++ app/test/settings/support/support_page_controller_test.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:common_domain_models/common_domain_models.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sharezone/support/support_page_controller.dart';
