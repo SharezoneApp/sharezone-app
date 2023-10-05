e6493a3a08bbf7d4938bfe65c82fdf09d3691032
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2023-10-05 18:54:47 +0200

    Add license headers.

--- app/test/homework/homework_dialog_test.dart
+++ app/test/homework/homework_dialog_test.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:analytics/analytics.dart';
 import 'package:analytics/null_analytics_backend.dart';
 import 'package:bloc_provider/bloc_provider.dart';
