575ad403840dea0a4ec99af93e2248f0e797d6a0
nilsreichardt <me@nils.re>
2024-01-06 20:51:18 +0100

    Recreate `website/macos` folder

--- website/macos/Runner/MainFlutterWindow.swift
+++ website/macos/Runner/MainFlutterWindow.swift
@@ -1,17 +1,9 @@
-// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
-// Licensed under the EUPL-1.2-or-later.
-//
-// You may obtain a copy of the Licence at:
-// https://joinup.ec.europa.eu/software/page/eupl
-//
-// SPDX-License-Identifier: EUPL-1.2
-
 import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
+    let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
