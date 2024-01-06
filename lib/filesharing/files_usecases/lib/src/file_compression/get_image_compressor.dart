fff31be60896febab0e4d220d92c361a812f5af1
nilsreichardt <me@nils.re>
2024-01-06 13:30:45 +0100

    Fix import

--- lib/filesharing/files_usecases/lib/src/file_compression/get_image_compressor.dart
+++ lib/filesharing/files_usecases/lib/src/file_compression/get_image_compressor.dart
@@ -9,7 +9,7 @@
 import 'image_compressor.dart';
 import 'implementation/stub_image_compressor.dart'
     // ignore: uri_does_not_exist
-    if (dart.library.io) 'implementation/mobile_native_image_compressor.dart'
+    if (dart.library.io) 'implementation/flutter_image_compressor.dart'
     as implementation;
 
 ImageCompressor getImageCompressor() {
