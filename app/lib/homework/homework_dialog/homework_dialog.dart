0431a5c1cc77325e871249722a06b7029d0e6d98
Nikhil Raj <112814295+gantanikhilraj@users.noreply.github.com>
2024-02-03 19:05:22 +0530

    Increased the size of the time text

--- app/lib/homework/homework_dialog/homework_dialog.dart
+++ app/lib/homework/homework_dialog/homework_dialog.dart
@@ -1190,7 +1190,12 @@ class _SubmissionsSwitchBase extends StatelessWidget {
                   },
                   trailing: Padding(
                     padding: const EdgeInsets.only(right: 8),
-                    child: Text(time.toString()),
+                    child: Text(
+                      time.toString(),
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                    ),
                   ),
                 )
               : Container(),
