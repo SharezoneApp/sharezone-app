e1fc94b49d33f3bd9ffbb0e87d3d130047cf1e1d
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-02-07 15:32:41 +0100

    Revert constants.dart

--- app/lib/main/constants.dart
+++ app/lib/main/constants.dart
@@ -6,7 +6,4 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-// Can be overridden for testing purposes.
-String? kDevelopmentStageOrNull =
-    kDevelopmentStage == "" ? null : kDevelopmentStage;
 const kDevelopmentStage = String.fromEnvironment('DEVELOPMENT_STAGE');
