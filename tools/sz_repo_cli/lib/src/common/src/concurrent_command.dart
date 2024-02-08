8beb89486abd032bf4ac1084694a331feb2f2c02
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-02-08 08:50:44 +0100

    Add `--only` option to sharezone cli. (#1292)
    The option allows to run a `ConcurrentCommand` only for the specified
    package names.
    
    E.g.: `sz analyze --only sz_repo_cli`, `sz test
    sharezone,abgabe_client_lib`.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced an option to execute tasks for specific packages only,
    enhancing task management efficiency.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- tools/sz_repo_cli/lib/src/common/src/concurrent_command.dart
+++ tools/sz_repo_cli/lib/src/common/src/concurrent_command.dart
@@ -15,6 +15,11 @@ import 'package:sz_repo_cli/src/common/common.dart';
 abstract class ConcurrentCommand extends CommandBase {
   ConcurrentCommand(super.context) {
     argParser
+      ..addOption(
+        'only',
+        help:
+            'Only run the task for the given package(s). Package names can be separated by comma. E.g. `--only package1` or `--only=package1,package2`.',
+      )
       ..addConcurrencyOption(defaultMaxConcurrency: defaultMaxConcurrency)
       ..addPackageTimeoutOption(
           defaultInMinutes: defaultPackageTimeout.inMinutes);
@@ -54,7 +59,25 @@ abstract class ConcurrentCommand extends CommandBase {
   ///
   /// Can be overridden to e.g. add a filter (`.where((package) =>
   /// package.hasTestDirectory`).
-  Stream<Package> get packagesToProcess => repo.streamPackages();
+  Stream<Package> get packagesToProcess {
+    var stream = repo.streamPackages();
+
+    final onlyPackageNames = _parseOnlyArg();
+    if (onlyPackageNames.isNotEmpty) {
+      stream =
+          stream.where((package) => onlyPackageNames.contains(package.name));
+    }
+
+    return stream;
+  }
+
+  List<String> _parseOnlyArg() {
+    final onlyArg = argResults!['only'] as String?;
+    if (onlyArg == null) {
+      return [];
+    }
+    return onlyArg.split(',');
+  }
 
   @override
   Future<void> run() async {
