c0426debb53d91e60152124b189a2a7b629acc05
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-02-22 21:02:57 +0100

    Add `sz pick-codemagic-goldens` command. (#1310)
    Adds the `sz pick-codemagic-goldens` command.  
    This command moves/renames golden files from codemagic so they can
    replace the old ones.
    
    We currently have to generate golden files on a mac. Developers on other
    platforms will have to upload their wrong golden files and then download
    the
    golden files that were generated in the failing Codemagic job.
    
    The files from Codemagic are named in the following pattern:
    * foo_testImage.png (The image generated by Codemagic)
    * foo_masterImage.png (The image that was checked into the repo)
    * foo_isolatedDiff.png
    * foo_maskedDiff.png
    
    This command will pick all the files that end with `_testImage.png`,
    remove
    the `_testImage` part and put them in a new folder called `corrected`.
    
    These images can then be taken by the developer to replace the old
    golden
    files with the same name.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a command to automate the process of updating golden files
    from Codemagic, streamlining the workflow for handling failing Codemagic
    jobs.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- tools/sz_repo_cli/lib/src/commands/commands.dart
+++ tools/sz_repo_cli/lib/src/commands/commands.dart
@@ -12,6 +12,7 @@ export 'src/deploy_web_app_command.dart';
 export 'src/do_stuff_command.dart';
 export 'src/exec_command.dart';
 export 'src/fix_comment_spacing_command.dart' show FixCommentSpacingCommand;
+export 'src/pick_codemagic_goldens_command.dart';
 export 'src/pub_command.dart';
 export 'src/pub_get_command.dart' show PubGetCommand;
 export 'src/test_command.dart';
