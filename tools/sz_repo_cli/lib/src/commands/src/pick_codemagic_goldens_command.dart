0da648a065eb2a1ea5d28caa6b7dd193c68f5ec8
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-02-22 18:42:58 +0100

    Fix license header

--- tools/sz_repo_cli/lib/src/commands/src/pick_codemagic_goldens_command.dart
+++ tools/sz_repo_cli/lib/src/commands/src/pick_codemagic_goldens_command.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Sharezone UG (haftungsbeschränkt)
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
 // Licensed under the EUPL-1.2-or-later.
 //
 // You may obtain a copy of the Licence at:
