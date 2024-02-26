c4da27237b86455d82f13aa17458c9405b48c4a8
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-02-26 17:43:20 +0100

    Replace `CalendricalEventType` with `EventType` enum.

--- app/test/timetable/timetable_bloc_test.dart
+++ app/test/timetable/timetable_bloc_test.dart
@@ -15,7 +15,6 @@ import 'package:date/weektype.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:group_domain_models/group_domain_models.dart';
 import 'package:sharezone/calendrical_events/models/calendrical_event.dart';
-import 'package:sharezone/calendrical_events/models/calendrical_event_types.dart';
 import 'package:sharezone/timetable/src/bloc/timetable_bloc.dart';
 import 'package:sharezone/timetable/src/models/lesson.dart';
 import 'package:sharezone/timetable/timetable_page/school_class_filter/school_class_filter_view.dart';
@@ -72,7 +71,7 @@ void main() {
         date: Date.today(),
         detail: '',
         eventID: 'eventId',
-        eventType: Meeting(),
+        eventType: EventType.event,
         latestEditor: '',
         sendNotification: false,
         title: 'title',
