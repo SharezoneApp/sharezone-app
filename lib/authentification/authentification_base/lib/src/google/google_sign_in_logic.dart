34166107d0b6e9b9a6c79ae766093af51bb57eff
nilsreichardt <me@nils.re>
2024-02-29 14:14:36 +0100

    Add note about android

--- lib/authentification/authentification_base/lib/src/google/google_sign_in_logic.dart
+++ lib/authentification/authentification_base/lib/src/google/google_sign_in_logic.dart
@@ -30,7 +30,7 @@ class GoogleSignInLogic {
     // without the possibility to select another account. This is a problem when
     // the user wasn't sure which Google account was the right one, signs in
     // with the wrong account, and then wants to sign in with the correct
-    // account.
+    // account. Especially on Android is this a problem.
     //
     // From: https://stackoverflow.com/a/58509980/8358501
     await _googleSignIn.disconnect();
