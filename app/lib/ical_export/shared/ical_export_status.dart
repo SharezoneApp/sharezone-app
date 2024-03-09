4fd4eda09d60ec2346af8bda21aadda3f91045fc
nilsreichardt <me@nils.re>
2024-03-10 00:38:27 +0100

    Add `ICalExportStatus`

---
+++ app/lib/ical_export/shared/ical_export_status.dart
@@ -0,0 +1,13 @@
+enum ICalExportStatus {
+  /// The export is in the process of being created.
+  creating,
+
+  /// The export is available for use.
+  available,
+
+  /// The export is locked and cannot be used.
+  ///
+  /// This could be because the Sharezone Plus subscription has expired. Once
+  /// the subscription is renewed, the export will be available again.
+  locked,
+}
