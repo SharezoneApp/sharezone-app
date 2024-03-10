e0bbf6a4301b1a9bcba310dfa5ba4239496b41ba
nilsreichardt <me@nils.re>
2024-03-10 23:47:09 +0100

    Add flavors (prod & dev) to our console

--- console/lib/main.dart
+++ console/lib/main.dart
@@ -9,7 +9,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sharezone_console/firebase_options.dart';
+import 'package:sharezone_console/firebase_options_prod.g.dart' as prod;
+import 'package:sharezone_console/firebase_options_dev.g.dart' as dev;
+import 'package:sharezone_console/flavor.dart';
 import 'package:sharezone_console/login_signup_page.dart';
 
 import 'home_page.dart';
@@ -17,9 +19,19 @@ import 'home_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final flavor = Flavor.fromEnvironment();
+  await _initFirebase(flavor);
 
   runApp(MyApp());
+}
+
+Future<void> _initFirebase(Flavor flavor) async {
+  await Firebase.initializeApp(
+    options: switch (flavor) {
+      Flavor.prod => prod.DefaultFirebaseOptions.currentPlatform,
+      Flavor.dev => dev.DefaultFirebaseOptions.currentPlatform,
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
