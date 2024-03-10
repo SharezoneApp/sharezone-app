f9f73d6f7c66af0aa0e8ef839bb32b693c1a919a
nilsreichardt <me@nils.re>
2024-03-10 19:51:10 +0100

    Show `_CreateCustomCourseSection`

--- app/lib/groups/src/pages/shared/course_template_page.dart
+++ app/lib/groups/src/pages/shared/course_template_page.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:bloc_provider/bloc_provider.dart';
 import 'package:common_domain_models/common_domain_models.dart';
 import 'package:flutter/material.dart';
@@ -35,10 +43,7 @@ class CourseTemplatePage extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: _CreateCustomCourseSection(
-        onTap: () => openCourseCreatePage(
-          context,
-          schoolClassId: schoolClassId,
-        ),
+        schoolClassId: schoolClassId,
       ),
     );
   }
@@ -86,7 +91,9 @@ class _CourseTemplatePageBodyState extends State<CourseTemplatePageBody> {
       onCreateCoursePressed: (template) =>
           bloc.submitWithCourseTemplate(template),
       bottom: widget.withCreateCustomCourseSection
-          ? _CreateCustomCourseSection()
+          ? _CreateCustomCourseSection(
+              schoolClassId: widget.schoolClassId,
+            )
           : null,
     );
   }
