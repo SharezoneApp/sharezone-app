76d86ef46f4d1472ccc0630997acfcd4eb00f266
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-03-13 10:54:58 +0100

    Create GradesTestController and use it in tests.

--- app/test/grades/grades_test.dart
+++ app/test/grades/grades_test.dart
@@ -1,24 +1,43 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sharezone/grades/subject_id.dart';
 
 void main() {
   group('grades', () {
     test('test name', () {
+      final controller = GradesTestController();
+
       final englisch = subjectWith(name: 'Englisch', withGrades: [
         gradeWith(value: 2),
         gradeWith(value: 3),
         gradeWith(value: 1),
       ]);
 
-      expect(englisch.getAverageGrade(), 2.0);
+      controller.addSubject(englisch);
+
+      expect(controller.getAverageGradeForSubject(englisch.id), 2.0);
     });
   });
 }
 
+class GradesTestController {
+  final List<Subject> _subjects = [];
+
+  void addSubject(Subject subject) {
+    _subjects.add(subject);
+  }
+
+  double getAverageGradeForSubject(SubjectId id) {
+    final subject = _subjects.firstWhere((subject) => subject.id == id);
+    return subject.getAverageGrade();
+  }
+}
+
 class Subject {
+  final SubjectId id;
   final String name;
   final List<Grade> grades;
 
-  Subject({required this.name, required this.grades});
+  Subject({required this.id, required this.name, required this.grades});
 
   double getAverageGrade() {
     return grades.map((grade) => grade.value).reduce((a, b) => a + b) /
@@ -33,7 +52,7 @@ class Grade {
 }
 
 Subject subjectWith({required String name, required List<Grade> withGrades}) {
-  return Subject(name: name, grades: withGrades);
+  return Subject(id: SubjectId(name), name: name, grades: withGrades);
 }
 
 Grade gradeWith({required int value}) {
