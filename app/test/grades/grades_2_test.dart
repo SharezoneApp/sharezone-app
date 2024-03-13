d312a6f135b3f9f54f6e47e64a2b59927e782fa4
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-03-13 12:44:31 +0100

    Change test to get average grade of two different subjects in term

--- app/test/grades/grades_2_test.dart
+++ app/test/grades/grades_2_test.dart
@@ -1,8 +1,9 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('grades', () {
-    test('get average grade of subject in term', () {
+    test('get average grade of two different subjects in term', () {
       final term = Term();
       final englisch = Subject('Englisch');
 
@@ -11,23 +12,34 @@ void main() {
       term.addGrade(1.0, toSubject: englisch.id);
 
       expect(term.getAverageGradeForSubject(englisch.id), 2.0);
+
+      final mathe = Subject('Mathe');
+      term.addSubject(mathe);
+      term.addGrade(2.0, toSubject: mathe.id);
+      term.addGrade(4.0, toSubject: mathe.id);
+
+      expect(term.getAverageGradeForSubject(mathe.id), 3.0);
     });
   });
 }
 
 class Term {
-  num total = 0;
-  num nrOfSubjects = 0;
+  final _subjects = <String, ({num total, int nrOfGrades})>{};
 
   void addGrade(double grade, {required String toSubject}) {
-    total += grade;
-    nrOfSubjects++;
+    final total = _subjects[toSubject]?.total ?? 0;
+    final nrOfGrades = _subjects[toSubject]?.nrOfGrades ?? 0;
+
+    _subjects[toSubject] = (total: total + grade, nrOfGrades: nrOfGrades + 1);
   }
 
   void addSubject(Subject subject) {}
 
   num getAverageGradeForSubject(String id) {
-    return total / nrOfSubjects;
+    final total = _subjects[id]!.total;
+    final nrOfGrades = _subjects[id]!.nrOfGrades;
+
+    return total / nrOfGrades;
   }
 }
 
