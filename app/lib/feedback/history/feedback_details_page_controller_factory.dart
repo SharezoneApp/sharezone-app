a44d106984ff29d194cf1ecdfa94c11fd8a1e743
nilsreichardt <me@nils.re>
2024-03-14 18:13:25 +0100

    Add lh

--- app/lib/feedback/history/feedback_details_page_controller_factory.dart
+++ app/lib/feedback/history/feedback_details_page_controller_factory.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:common_domain_models/common_domain_models.dart';
 import 'package:crash_analytics/crash_analytics.dart';
 import 'package:sharezone/feedback/history/feedback_details_page_controller.dart';
