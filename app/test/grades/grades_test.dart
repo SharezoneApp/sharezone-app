f86c7d642dfd5fef4fa938f611617861387066f2
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-03-15 14:22:32 +0100

    Add more abstract test 'The calculated grade of the term is the average of subject grades by default'

--- app/test/grades/grades_test.dart
+++ app/test/grades/grades_test.dart
@@ -2,190 +2,119 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sharezone/grades/subject_id.dart';
 import 'package:sharezone/grades/term_id.dart';
+import 'package:test_randomness/test_randomness.dart';
+
+import 'grades_2_test.dart';
 
 void main() {
   group('grades', () {
-    test('test name', () {
+    test(
+        'The calculated grade of the term is the average of subject grades by default',
+        () {
       final controller = GradesTestController();
 
-      final term = termWith(name: '1. Halbjahr');
-      controller.addTerm(term);
-
-      final englisch = subjectWith(name: 'Englisch', withGrades: [
-        gradeWith(value: 2),
-        gradeWith(value: 3),
-        gradeWith(value: 1),
+      final term = termWith(name: '1. Halbjahr', subjects: [
+        subjectWith(
+            id: SubjectId('Deutsch'),
+            name: 'Deutsch',
+            grades: [gradeWith(value: 3.0), gradeWith(value: 1.0)]),
+        subjectWith(
+            id: SubjectId('Englisch'),
+            name: 'Englisch',
+            grades: [gradeWith(value: 2.0), gradeWith(value: 4.0)]),
       ]);
+      controller.createTerm(term);
 
-      controller.addSubjectToTerm(englisch, term.id);
-
-      expect(controller.getAverageGradeForSubject(englisch.id), 2.0);
-    });
-
-    test('term', () {
-      final controller = GradesTestController();
-
-      final term = termWith(name: '1. Halbjahr');
-      controller.addTerm(term);
-
-      final englisch = subjectWith(name: 'Englisch', withGrades: [
-        gradeWith(value: 2),
-        gradeWith(value: 4),
-      ]);
-
-      final mathe = subjectWith(name: 'Mathe', withGrades: [
-        gradeWith(value: 1),
-        gradeWith(value: 3),
-      ]);
-
-      controller.addSubjectToTerm(englisch, term.id);
-      controller.addSubjectToTerm(mathe, term.id);
-
-      expect(controller.getAverageGradeWithAllSubjects(term.id), 2.5);
-    });
-
-    test('create two different terms and get average note', () {
-      final controller = GradesTestController();
-
-      final firstTerm = termWith(name: '1. Halbjahr');
-      final secondTerm = termWith(name: '2. Halbjahr');
-
-      controller.addTerm(firstTerm);
-      controller.addTerm(secondTerm);
-
-      final englisch = subjectWith(name: 'Englisch', withGrades: [
-        gradeWith(value: 2),
-      ]);
-
-      final mathe = subjectWith(name: 'Mathe', withGrades: [
-        gradeWith(value: 4),
-      ]);
-
-      controller.addSubjectToTerm(englisch, firstTerm.id);
-      controller.addSubjectToTerm(mathe, firstTerm.id);
-
-      final deutsch = subjectWith(name: 'Deutsch', withGrades: [
-        gradeWith(value: 1),
-      ]);
-      final informatik = subjectWith(name: 'Informatik', withGrades: [
-        gradeWith(value: 3),
-      ]);
-
-      controller.addSubjectToTerm(deutsch, secondTerm.id);
-      controller.addSubjectToTerm(informatik, secondTerm.id);
-
-      expect(controller.getAverageGradeWithAllSubjects(firstTerm.id), 3);
-      expect(controller.getAverageGradeWithAllSubjects(secondTerm.id), 2);
+      expect(controller.term(term.id).calculatedGrade, 2.5);
     });
   });
 }
 
 class GradesTestController {
-  final List<Subject> _subjects = [];
-  List<Term> _terms = [];
+  late Term _term;
 
-  void addSubject(Subject subject) {
-    _subjects.add(subject);
+  void createTerm(TestTerm testTerm) {
+    _term = Term();
+    for (var subject in testTerm.subjects.values) {
+      _term = _term.addSubject(Subject(subject.id.id));
+      for (var grade in subject.grades) {
+        _term = _term
+            .subject(subject.id.id)
+            .addGrade(Grade(id: GradeId(randomAlpha(4)), value: grade.value));
+      }
+    }
   }
 
-  double getAverageGradeForSubject(SubjectId id) {
-    final subject = _subjects.firstWhere((subject) => subject.id == id);
-    return subject.getAverageGrade();
-  }
-
-  Term getTerm(TermId id) {
-    return _terms.firstWhere((term) => term.id == id);
-  }
-
-  num getAverageGradeWithAllSubjects(TermId termId) {
-    final _term = getTerm(termId);
-
-    return _term.getAverageGrade();
-  }
-
-  void addSubjectToTerm(Subject subject, TermId id) {
-    final term = _terms.where((term) => term.id == id).first;
-    final subjectWithTerm = subject.copyWith(termId: id);
-    term.addSubject(subjectWithTerm);
-
-    addSubject(subjectWithTerm);
-  }
-
-  void addTerm(Term term) {
-    _terms.add(term);
+  TermResult term(TermId id) {
+    return TermResult(_term.getTermGrade());
   }
 }
 
-Term termWith({required String name}) {
-  return Term(id: TermId(name), name: name);
+class TermResult {
+  final num calculatedGrade;
+
+  TermResult(this.calculatedGrade);
 }
 
-class Term {
+TestTerm termWith({
+  required String name,
+  required List<TestSubject> subjects,
+}) {
+  return TestTerm(
+    id: TermId('1'),
+    name: name,
+    subjects: IMap.fromEntries(subjects.map((s) => MapEntry(s.id, s))),
+  );
+}
+
+class TestTerm {
   final TermId id;
   final String name;
-  IList<Subject> subjects;
+  final IMap<SubjectId, TestSubject> subjects;
 
-  Term({
+  TestTerm({
     required this.id,
     required this.name,
-    this.subjects = const IListConst([]),
+    required this.subjects,
   });
-
-  void addSubject(Subject subject) {
-    subjects = subjects.add(subject);
-  }
-
-  double getAverageGrade() {
-    return subjects
-            .map((subject) => subject.getAverageGrade())
-            .reduce((a, b) => a + b) /
-        subjects.length;
-  }
 }
 
-class Subject {
+TestSubject subjectWith({
+  required SubjectId id,
+  required String name,
+  required List<TestGrade> grades,
+}) {
+  return TestSubject(
+    id: id,
+    name: name,
+    grades: IList(grades),
+  );
+}
+
+class TestSubject {
   final SubjectId id;
   final String name;
-  final List<Grade> grades;
-  final TermId? termId;
+  final IList<TestGrade> grades;
 
-  Subject({
+  TestSubject({
     required this.id,
     required this.name,
     required this.grades,
-    this.termId,
   });
-
-  double getAverageGrade() {
-    return grades.map((grade) => grade.value).reduce((a, b) => a + b) /
-        grades.length;
-  }
-
-  Subject copyWith({
-    SubjectId? id,
-    String? name,
-    List<Grade>? grades,
-    TermId? termId,
-  }) {
-    return Subject(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      grades: grades ?? this.grades,
-      termId: termId ?? this.termId,
-    );
-  }
 }
 
-class Grade {
-  final int value;
-
-  Grade({required this.value});
+TestGrade gradeWith({
+  required double value,
+}) {
+  return TestGrade(
+    value: value,
+  );
 }
 
-Subject subjectWith({required String name, required List<Grade> withGrades}) {
-  return Subject(id: SubjectId(name), name: name, grades: withGrades);
-}
+class TestGrade {
+  final double value;
 
-Grade gradeWith({required int value}) {
-  return Grade(value: value);
+  TestGrade({
+    required this.value,
+  });
 }
