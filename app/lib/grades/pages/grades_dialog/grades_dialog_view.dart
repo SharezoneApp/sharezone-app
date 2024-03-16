ef7fc43257c92b1f9b43763a1337afc8e7cd490b
nilsreichardt <me@nils.re>
2024-03-16 18:04:57 +0100

    Add `GradesDialogView`

---
+++ app/lib/grades/pages/grades_dialog/grades_dialog_view.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+class GradesDialogView {
+  /// The selected grading system.
+  ///
+  /// Defaults to "1 - 6 (+-)".
+  final String selectedGradingSystem;
+
+  /// The selected subject.
+  final String? selectedSubject;
+
+  /// The selected date in the format "Sat, Mar 16, 2024".
+  ///
+  /// Defaults to the current date.
+  final String selectedDate;
+
+  /// The selected grading type.
+  ///
+  /// Defaults to written exam.
+  final String selectedGradingType;
+
+  final bool integrateGradeIntoSubjectGrade;
+  final String? selectedTerm;
+  final String? topic;
+  final String? notes;
+
+  const GradesDialogView({
+    required this.selectedGradingSystem,
+    required this.selectedSubject,
+    required this.selectedDate,
+    required this.selectedGradingType,
+    required this.selectedTerm,
+    required this.topic,
+    required this.notes,
+    required this.integrateGradeIntoSubjectGrade,
+  });
+}
