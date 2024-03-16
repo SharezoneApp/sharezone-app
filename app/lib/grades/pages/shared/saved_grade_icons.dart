f97510d232b035da4098d0d921b8b31e5e722a53
nilsreichardt <me@nils.re>
2024-03-16 18:04:28 +0100

    Add `SavedGradeIcons`

---
+++ app/lib/grades/pages/shared/saved_grade_icons.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+/// Icons used in the saved grade dialog and the saved grade details page.
+class SavedGradeIcons {
+  static const value = Icon(Icons.grade);
+  static const gradingSystem = Icon(FontAwesomeIcons.calculator);
+  static const subject = Icon(Icons.book);
+  static const date = Icon(Icons.today);
+  static const gradingType = Icon(Icons.label);
+  static const term = Icon(Icons.calendar_today);
+  static const integrateGradeIntoSubjectGrade = Icon(Icons.science);
+  static const topic = Icon(Icons.topic);
+  static const notes = Icon(Icons.notes);
+}
