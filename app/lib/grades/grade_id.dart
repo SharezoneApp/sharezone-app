50c68c5c58b219ebc6ffcdc0a9a959a60bd19437
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-03-20 10:29:57 +0100

    Fix Id classes

--- app/lib/grades/grade_id.dart
+++ app/lib/grades/grade_id.dart
@@ -1,5 +1,5 @@
 import 'package:common_domain_models/common_domain_models.dart';
 
 class GradeId extends Id {
-  GradeId(String id) : super(id, 'GradeId');
+  GradeId(super.id);
 }
--- app/lib/grades/term_id.dart
+++ app/lib/grades/term_id.dart
@@ -1,5 +1,5 @@
 import 'package:common_domain_models/common_domain_models.dart';
 
 class TermId extends Id {
-  TermId(String id) : super(id, 'TermId');
+  TermId(super.id);
 }
