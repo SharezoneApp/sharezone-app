21d9f102c5ebd05ba8be18cf793525fa3afa508c
Jonas <--help>
2024-04-09 17:25:02 +0200

    Fix comment

--- app/lib/grades/pages/grades_page/grades_page_controller.dart
+++ app/lib/grades/pages/grades_page/grades_page_controller.dart
@@ -102,7 +102,7 @@ String displayGrade(GradeValue? grade) {
 
   if (grade.displayableGrade != null) return grade.displayableGrade!;
   if(!grade.asNum.hasDecimals) return withSuffix('${grade.asNum}');
-  // Only show two decimals: "2.23563" -> "2.24"
+  // Only show two decimals: "2.23563" -> "2.23"
   var gradeString = grade.asDouble.toStringAsFixedWithoutRounding(2);
   return withSuffix(gradeString);
 }
