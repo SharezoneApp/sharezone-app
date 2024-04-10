0de83f8a1ae4d3676a0ee44f011257d62c638ece
Jonas <--help>
2024-04-10 13:56:53 +0200

    Add commented out decimalsAllowed test logic

--- app/test/grades/grading_systems_test.dart
+++ app/test/grades/grading_systems_test.dart
@@ -502,6 +502,19 @@ void main() {
           );
         }
 
+        // This is too much work right now, readd in future
+        // Test that error is thrown if a number with decimals is added to a grading system that doesn't allow them
+        //if (!possibleGrades.decimalsAllowed) {
+        //  final gradeWithDecimals = possibleGrades.max - 0.5;
+        //
+        //  expect(
+        //      () => addGrade(gradeWithDecimals.toString()),
+        //      throwsA(InvalidGradeValueException(
+        //        gradeInput: '$gradeWithDecimals',
+        //        gradingSystem: gradingSystem,
+        //      )));
+        //}
+
         final aBitTooHigh =
             possibleGrades.max + (possibleGrades.decimalsAllowed ? 0.01 : 1);
         final aBitTooLow =
