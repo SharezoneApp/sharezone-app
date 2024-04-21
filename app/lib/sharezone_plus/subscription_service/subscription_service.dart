bba3cc1ad6090085dc105f61bf14bd139c67558a
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-04-21 19:25:19 +0200

    Remove `SubscriptionEnabledFlag` and `GradesEnabledFlag`. (#1485)

--- app/lib/sharezone_plus/subscription_service/subscription_service.dart
+++ app/lib/sharezone_plus/subscription_service/subscription_service.dart
@@ -7,26 +7,18 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 import 'package:clock/clock.dart';
-import 'package:sharezone/sharezone_plus/subscription_service/subscription_flag.dart';
 import 'package:user/user.dart';
 
 class SubscriptionService {
   final Stream<AppUser?> user;
   final Clock clock;
-  final SubscriptionEnabledFlag isSubscriptionEnabledFlag;
 
   late AppUser? _user;
-  bool _isEnabled = false;
 
   SubscriptionService({
     required this.user,
     required this.clock,
-    required this.isSubscriptionEnabledFlag,
   }) {
-    _isEnabled = isSubscriptionEnabledFlag.isEnabled;
-    isSubscriptionEnabledFlag.addListener(() {
-      _isEnabled = isSubscriptionEnabledFlag.isEnabled;
-    });
     user.listen((event) {
       _user = event;
     });
@@ -35,31 +27,20 @@ class SubscriptionService {
   bool isSubscriptionActive([AppUser? appUser]) {
     appUser ??= _user;
 
-    // Subscriptions feature is disabled, so every feature is unlocked.
-    if (!_isEnabled) return true;
-
     if (appUser?.subscription == null) return false;
     return clock.now().isBefore(appUser!.subscription!.expiresAt);
   }
 
   Stream<bool> isSubscriptionActiveStream() {
-    // Subscriptions feature is disabled, so every feature is unlocked.
-    if (!_isEnabled) return Stream.value(true);
     return user.map(isSubscriptionActive);
   }
 
   bool hasFeatureUnlocked(SharezonePlusFeature feature) {
-    // Subscriptions feature is disabled, so every feature is unlocked.
-    if (!_isEnabled) return true;
-
     if (!isSubscriptionActive()) return false;
     return _user!.subscription!.tier.hasUnlocked(feature);
   }
 
   Stream<bool> hasFeatureUnlockedStream(SharezonePlusFeature feature) {
-    // Subscriptions feature is disabled, so every feature is unlocked.
-    if (!_isEnabled) return Stream.value(true);
-
     return user.map((event) => hasFeatureUnlocked(feature));
   }
 }
