1d7792053d6e0bdf0ffb5c70bc056b5763e4245a
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-04-23 17:50:50 +0200

    Add fetchUserData

--- website/lib/sharezone_plus/sharezone_plus_page.dart
+++ website/lib/sharezone_plus/sharezone_plus_page.dart
@@ -6,6 +6,7 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sharezone_plus_page_ui/sharezone_plus_page_ui.dart';
@@ -16,6 +17,21 @@ import 'package:sharezone_widgets/sharezone_widgets.dart';
 final isSharezonePlusPageEnabledFlag =
     kDebugMode || Flavor.fromEnvironment() == Flavor.dev;
 
+typedef UserData = ({String username, String userId});
+
+Future<UserData?> fetchUserData(String? token) async {
+  if (token == null) {
+    return null;
+  }
+  const link =
+      "https://europe-west1-sharezone-debug.cloudfunctions.net/getDataForPlusWebsiteBuyToken";
+  final response = await Dio().post(link, data: {'token': token});
+  final dataMap = response.data as Map;
+  final username = dataMap['username'] as String;
+  final userId = dataMap['userId'] as String;
+  return (username: username, userId: userId);
+}
+
 class SharezonePlusPage extends StatelessWidget {
   const SharezonePlusPage({super.key});
 
@@ -23,31 +39,52 @@ class SharezonePlusPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const PageTemplate(
-      children: [
-        MaxWidthConstraintBox(
-          maxWidth: 750,
-          child: Column(
-            children: [
-              SharezonePlusPageHeader(),
-              SizedBox(height: 18),
-              WhyPlusSharezoneCard(),
-              SizedBox(height: 18),
-              SharezonePlusAdvantages(
-                isHomeworkDoneListsFeatureVisible: true,
-                isHomeworkReminderFeatureVisible: true,
+    final map = ModalRoute.of(context)!.settings.arguments as Map;
+    final urlToken = map['token'] as String?;
+    return FutureBuilder<UserData?>(
+      future: fetchUserData(urlToken),
+      builder: (context, snapshot) {
+        final data = snapshot.data;
+        return PageTemplate(
+          children: [
+            MaxWidthConstraintBox(
+              maxWidth: 750,
+              child: Column(
+                children: [
+                  if (snapshot.hasError) Text('Error: ${snapshot.error}'),
+                  if (data != null)
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(17.5),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [Text('Name: ${data.username}')],
+                          )
+                        ],
+                      ),
+                    ),
+                  SizedBox(height: 18),
+                  WhyPlusSharezoneCard(),
+                  SizedBox(height: 18),
+                  SharezonePlusAdvantages(
+                    isHomeworkDoneListsFeatureVisible: true,
+                    isHomeworkReminderFeatureVisible: true,
+                  ),
+                  SizedBox(height: 18),
+                  _SubscribeSection(),
+                  SizedBox(height: 32),
+                  SharezonePlusFaq(),
+                  SizedBox(height: 18),
+                  SharezonePlusSupportNote(),
+                  SizedBox(height: 32),
+                ],
               ),
-              SizedBox(height: 18),
-              _SubscribeSection(),
-              SizedBox(height: 32),
-              SharezonePlusFaq(),
-              SizedBox(height: 18),
-              SharezonePlusSupportNote(),
-              SizedBox(height: 32),
-            ],
-          ),
-        ),
-      ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
