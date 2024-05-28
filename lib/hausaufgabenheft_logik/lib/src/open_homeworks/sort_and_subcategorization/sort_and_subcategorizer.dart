91c13eccce138b7744b0b8acc2883dcac881dabf
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-05-28 17:03:41 +0200

    Merge files into sort_and_subcategorizer.dart

--- lib/hausaufgabenheft_logik/lib/src/open_homeworks/sort_and_subcategorization/sort_and_subcategorizer.dart
+++ lib/hausaufgabenheft_logik/lib/src/open_homeworks/sort_and_subcategorization/sort_and_subcategorizer.dart
@@ -6,22 +6,119 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import 'package:hausaufgabenheft_logik/src/homework_list_extensions.dart';
-import 'package:hausaufgabenheft_logik/src/open_homeworks/sort_and_subcategorization/sort/src/sort.dart';
-import 'package:hausaufgabenheft_logik/src/models/homework.dart';
-import 'package:hausaufgabenheft_logik/src/open_homeworks/views/homework_section_view.dart';
-
-import 'subcategorizer.dart';
+import 'package:hausaufgabenheft_logik/color.dart';
+import 'package:hausaufgabenheft_logik/hausaufgabenheft_logik.dart';
+import 'package:hausaufgabenheft_logik/src/views/student_homework_view_factory.dart';
 
 class HomeworkSortAndSubcategorizer {
-  Subcategorizer Function(Sort<HomeworkReadModel> sortToMatch)
-      getMatchingSubcategorizer;
-
-  HomeworkSortAndSubcategorizer(this.getMatchingSubcategorizer);
+  final Color defaultColor;
+  final Date Function() getCurrentDate;
+  HomeworkSortAndSubcategorizer({
+    required this.defaultColor,
+    required this.getCurrentDate,
+  });
 
   List<HomeworkSectionView> sortAndSubcategorize(
       List<HomeworkReadModel> homeworks, Sort<HomeworkReadModel> sort) {
     homeworks.sortWith(sort);
-    return getMatchingSubcategorizer(sort).subcategorize(homeworks);
+    return SubcategorizerFactory(StudentHomeworkViewFactory(
+      defaultColorValue: defaultColor.value,
+      getCurrentDate: getCurrentDate,
+    )).getMatchingSubcategorizer(sort).subcategorize(homeworks);
+  }
+}
+
+class SubcategorizerFactory {
+  final StudentHomeworkViewFactory _viewFactory;
+
+  SubcategorizerFactory(this._viewFactory);
+
+  Subcategorizer getMatchingSubcategorizer(Sort sort) {
+    if (sort is SubjectSmallestDateAndTitleSort) {
+      return SubjectSubcategeorizer(_viewFactory);
+    } else if (sort is SmallestDateSubjectAndTitleSort) {
+      return TodoDateSubcategorizer(sort.getCurrentDate(), _viewFactory);
+    } else {
+      throw UnimplementedError('No matching Subcategorizer for $sort');
+    }
+  }
+}
+
+abstract class Subcategorizer {
+  List<HomeworkSectionView> subcategorize(List<HomeworkReadModel> homeworks);
+}
+
+class SubjectSubcategeorizer extends Subcategorizer {
+  final StudentHomeworkViewFactory _viewFactory;
+
+  SubjectSubcategeorizer(this._viewFactory);
+
+  @override
+  List<HomeworkSectionView> subcategorize(List<HomeworkReadModel> homeworks) {
+    final subjects = homeworks.getDistinctOrderedSubjects();
+    final homeworkSections = <HomeworkSectionView>[];
+    for (final subject in subjects) {
+      final List<HomeworkReadModel> homeworksWithSubject =
+          homeworks.where((h) => h.subject == subject).toList();
+
+      final homeworkViewsWithSubject =
+          homeworksWithSubject.map((h) => _viewFactory.createFrom(h)).toList();
+
+      homeworkSections
+          .add(HomeworkSectionView(subject.name, homeworkViewsWithSubject));
+    }
+    return homeworkSections;
+  }
+}
+
+class TodoDateSubcategorizer extends Subcategorizer {
+  final Date currentDate;
+  final StudentHomeworkViewFactory _viewFactory;
+
+  TodoDateSubcategorizer(this.currentDate, this._viewFactory);
+
+  @override
+  List<HomeworkSectionView> subcategorize(List<HomeworkReadModel> homeworks) {
+    final latestHomeworkList = homeworks;
+    final now = currentDate;
+    final tomorrow = now.addDays(1);
+    final in2Days = tomorrow.addDays(1);
+
+    final List<HomeworkReadModel> overdueHomework = latestHomeworkList
+        .where((h) => Date.fromDateTime(h.todoDate) < now)
+        .toList();
+    final List<HomeworkReadModel> todayHomework = latestHomeworkList
+        .where((h) => Date.fromDateTime(h.todoDate) == now)
+        .toList();
+    final List<HomeworkReadModel> tomorrowHomework = latestHomeworkList
+        .where((h) => Date.fromDateTime(h.todoDate) == tomorrow)
+        .toList();
+    final List<HomeworkReadModel> in2DaysHomework = latestHomeworkList
+        .where((h) => Date.fromDateTime(h.todoDate) == in2Days)
+        .toList();
+    final List<HomeworkReadModel> futureHomework = latestHomeworkList
+        .where((h) => Date.fromDateTime(h.todoDate) > in2Days)
+        .toList();
+
+    final overdueSec = HomeworkSectionView.fromModels(
+        'Überfällig', overdueHomework, _viewFactory);
+    final todaySec =
+        HomeworkSectionView.fromModels('Heute', todayHomework, _viewFactory);
+    final tomorrowSec = HomeworkSectionView.fromModels(
+        'Morgen', tomorrowHomework, _viewFactory);
+    final inTwoDaysSec = HomeworkSectionView.fromModels(
+        'Übermorgen', in2DaysHomework, _viewFactory);
+    final afterTwoDaysSec =
+        HomeworkSectionView.fromModels('Später', futureHomework, _viewFactory);
+
+    final sections = [
+      overdueSec,
+      todaySec,
+      tomorrowSec,
+      inTwoDaysSec,
+      afterTwoDaysSec
+    ];
+
+    return sections.where((section) => section.isNotEmpty).toList();
   }
 }
--- lib/hausaufgabenheft_logik/lib/src/setup/create_homework_page_bloc.dart
+++ lib/hausaufgabenheft_logik/lib/src/setup/create_homework_page_bloc.dart
@@ -7,13 +7,13 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 import 'package:clock/clock.dart';
+import 'package:hausaufgabenheft_logik/color.dart';
 import 'package:hausaufgabenheft_logik/src/homework_completion/homework_page_completion_dispatcher.dart';
 import 'package:hausaufgabenheft_logik/src/student_homework_page_bloc/homework_sorting_cache.dart';
 
 import '../completed_homeworks/views/completed_homework_list_view_factory.dart';
 import '../models/models.dart';
 import '../open_homeworks/sort_and_subcategorization/sort_and_subcategorizer.dart';
-import '../open_homeworks/sort_and_subcategorization/subcategorizer_factory.dart';
 import '../open_homeworks/views/open_homework_list_view_factory.dart';
 import '../student_homework_page_bloc/student_homework_page_bloc.dart';
 import '../views/student_homework_view_factory.dart';
@@ -22,13 +22,16 @@ import 'dependencies.dart';
 
 HomeworkPageBloc createHomeworkPageBloc(
     HausaufgabenheftDependencies dependencies, HausaufgabenheftConfig config) {
+  getCurrentDate() => Date.now();
+
   final viewFactory = StudentHomeworkViewFactory(
       defaultColorValue: config.defaultCourseColorValue);
-  final subcategorizerFactory = SubcategorizerFactory(viewFactory);
   final sortAndSubcategorizer = HomeworkSortAndSubcategorizer(
-      subcategorizerFactory.getMatchingSubcategorizer);
+    defaultColor: Color(config.defaultCourseColorValue),
+    getCurrentDate: getCurrentDate,
+  );
   final openHomeworkListViewFactory =
-      OpenHomeworkListViewFactory(sortAndSubcategorizer, () => Date.now());
+      OpenHomeworkListViewFactory(sortAndSubcategorizer, getCurrentDate);
 
   final completedHomeworkListViewFactory =
       CompletedHomeworkListViewFactory(viewFactory);
