a25bdb559b2e714b8946facd6a27508acc88989a
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-06-18 17:35:45 +0200

    Add license headers

--- app/lib/homework/teacher/src/teacher_src.dart
+++ app/lib/homework/teacher/src/teacher_src.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 export 'teacher_open_homework_list.dart';
 export 'teacher_homework_tile.dart';
 export 'teacher_homework_bottom_action_bar.dart';
