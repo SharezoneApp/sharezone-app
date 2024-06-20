27ae276837567e2aa44d06b25791b37c639cdbd2
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2024-06-20 18:15:38 +0200

    Add comment

--- lib/hausaufgabenheft_logik/lib/src/firebase/src/firestore_teacher_and_parents_homework_page_api.dart
+++ lib/hausaufgabenheft_logik/lib/src/firebase/src/firestore_teacher_and_parents_homework_page_api.dart
@@ -37,6 +37,7 @@ class FirestoreTeacherAndParentsHomeworkPageApi
   Stream<IList<TeacherHomeworkReadModel>> loadHomeworks(int numberOfHomeworks) {
     return _homeworkCollection
         .where('assignedUserArrays.allAssignedUids', arrayContains: uid)
+        // TODO: Change to todoUntil
         .orderBy('createdOn', descending: true)
         .limit(numberOfHomeworks)
         .snapshots()
