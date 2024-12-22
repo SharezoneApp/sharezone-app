efbfe0a6db08f875ca3c859a630250327422dbb7
nilsreichardt <me@nils.re>
2024-12-22 23:40:35 +0100

    Add license header

--- lib/sharezone_localizations/lib/src/app_locale_gateway.dart
+++ lib/sharezone_localizations/lib/src/app_locale_gateway.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 import 'package:sharezone_localizations/sharezone_localizations.dart';
 
 /// Gateway to store and retrieve the locale of the app.
