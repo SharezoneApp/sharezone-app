f0b2eacd9158dc957b7c04402ceb6312e751e4ba
nilsreichardt <me@nils.re>
2024-12-22 22:42:32 +0100

    Format comment in `SharezoneLocalizationsContextExtension`

--- lib/sharezone_localizations/lib/src/context_extension.dart
+++ lib/sharezone_localizations/lib/src/context_extension.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:sharezone_localizations/sharezone_localizations.dart';
 
-/// Extension on BuildContext to provide easy access to the SharezoneLocalizations
 extension SharezoneLocalizationsContextExtension on BuildContext {
-  /// Requires the SharezoneLocalizations to be available in the context, otherwise it will throw an exception.
-  /// Add [SharezoneLocalizations.delegate] to the underlying App localizationsDelegates, for access
-  /// in the context of the app.
+  /// Extension on [BuildContext] to access the [SharezoneLocalizations] object.
+  ///
+  /// Requires the SharezoneLocalizations to be available in the context,
+  /// otherwise it will throw an exception. Add
+  /// [SharezoneLocalizations.delegate] to the underlying App
+  /// localizationsDelegates, for access in the context of the app.
   SharezoneLocalizations get sl => SharezoneLocalizations.of(this)!;
 }
