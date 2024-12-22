7e3077edaaa6f668cb706a4e44c56bf20495ba34
nilsreichardt <me@nils.re>
2024-12-22 22:21:07 +0100

    Add `getNativeName`, `getTranslatedName`, `toMap` and `fromMap` to `AppLocales`

--- lib/sharezone_localizations/lib/src/app_locales.dart
+++ lib/sharezone_localizations/lib/src/app_locales.dart
@@ -1,18 +1,84 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:sharezone_localizations/sharezone_localizations.dart';
 
+/// A wrapper around [Locale] to provide differentiation between the system
+/// locale and the user selected locale.
 enum AppLocales {
   system,
   en,
   de;
 
   /// Converts the enum value to a [Locale] object.
-  /// You can access the system locale using [PlatformDispatcher.instance.locale].
-  Locale toLocale({required Locale systemLocale}) {
+  Locale toLocale() {
     return switch (this) {
-      system => systemLocale,
+      system => getSystemLocale(),
       en => SharezoneAppLocales.en,
       de => SharezoneAppLocales.de,
     };
+  }
+
+  /// Returns the name of the locale in the native language, e.g. "Deutsch" for
+  /// the [AppLocales.de] enum value.
+  String getNativeName(BuildContext context) {
+    return switch (this) {
+      system => context.sl.language_system_name,
+      en => 'English',
+      de => 'Deutsch',
+    };
+  }
+
+  /// Returns the name of the locale in the currently selected language, e.g.
+  /// "German" for the [AppLocales.de] enum value when the app is in English.
+  String getTranslatedName(BuildContext context) {
+    return switch (this) {
+      system => context.sl.language_system_name,
+      en => context.sl.language_en_name,
+      de => context.sl.language_de_name,
+    };
+  }
+
+  /// The system-reported default locale of the device.
+  static Locale getSystemLocale() {
+    return PlatformDispatcher.instance.locale;
+  }
+
+  static AppLocales fromMap(Map<String, dynamic>? map) {
+    if (map == null || map['isSystem'] as bool) {
+      return system;
+    }
+    return fromLanguageTag(map['languageTag']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'isSystem': isSystem(),
+      'languageTag': _toLanguageTag(),
+    };
+  }
+
+  bool isSystem() {
+    return this == system;
+  }
+
+  /// Returns a syntactically valid Unicode BCP47 Locale Identifier.
+  ///
+  /// Some examples of such identifiers: "en", "es-419", "hi-Deva-IN" and
+  /// "zh-Hans-CN". See http://www.unicode.org/reports/tr35/ for technical
+  /// details.
+  String _toLanguageTag() {
+    return toLocale().toLanguageTag();
+  }
+
+  /// Returns the enum value for the given language tag.
+  ///
+  /// If the language tag is not supported, the system locale is returned.
+  static AppLocales fromLanguageTag(String languageTag) {
+    final languageCode = languageTag.split('-').first;
+    return AppLocales.values.firstWhere(
+      (element) => element.name == languageCode,
+      orElse: () => system,
+    );
   }
 }
 
