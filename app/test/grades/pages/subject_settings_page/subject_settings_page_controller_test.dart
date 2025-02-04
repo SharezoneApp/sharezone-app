21f035994057c586a20f919da3738627cc54ed31
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2025-02-04 12:05:28 +0100

    Add `subject_settings_page_controller_test.dart` with test: 'returns no weights if term has no default weights and subject has no weights'

---
+++ app/test/grades/pages/subject_settings_page/subject_settings_page_controller_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sharezone/grades/grades_service/grades_service.dart';
+import 'package:sharezone/grades/pages/subject_settings_page/subject_settings_page_controller.dart';
+
+import '../../grades_test_common.dart';
+
+void main() {
+  group('$SubjectSettingsPageController', () {
+    test(
+        'returns no weights if term has no default weights and subject has no weights',
+        () async {
+      final testController = GradesTestController();
+      const termId = TermId('1');
+      testController
+          .createTerm(termWith(id: termId, gradeTypeWeights: {}, subjects: [
+        subjectWith(
+          id: const SubjectId('maths'),
+          gradeTypeWeights: {},
+          // Subjects need a grade to be really created/assigned to the term.
+          grades: [gradeWith()],
+        ),
+      ]));
+
+      final controller = SubjectSettingsPageController(
+        subRef: testController.service
+            .term(termId)
+            .subject(const SubjectId('maths')),
+        gradesService: testController.service,
+      );
+
+      expect(controller.view.weights, isEmpty);
+    });
+  });
+}
