f350f95bdfb99ac2bd40b598c7a173a172a3fe67
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2025-02-21 22:06:59 +0100

    Add test: 'if there is no active term then the default $GradingSystem is ${GradingSystem.oneToSixWithPlusAndMinus}'

--- app/test/grades/pages/grades_dialog/grades_dialog_controller_test.dart
+++ app/test/grades/pages/grades_dialog/grades_dialog_controller_test.dart
@@ -3,8 +3,11 @@ import 'package:crash_analytics/crash_analytics.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:sharezone/grades/grades_service/grades_service.dart';
 import 'package:sharezone/grades/pages/grades_dialog/grades_dialog_controller.dart';
+
+import '../../../../test_goldens/grades/pages/create_term_page/create_term_page_test.mocks.dart';
 
 class MockCrashAnalytics extends Mock implements CrashAnalytics {}
 
@@ -28,6 +31,16 @@ void main() {
         analytics: analytics,
       );
     });
+
+    test(
+      'if there is no active term then the default $GradingSystem is ${GradingSystem.oneToSixWithPlusAndMinus}',
+      () {
+        expect(
+          controller.view.selectedGradingSystem,
+          GradingSystem.oneToSixWithPlusAndMinus,
+        );
+      },
+    );
 
     test(
       '.save throws correct $InvalidFieldsSaveGradeException when no fields are filled out and save is pressed',
