1bb7c426e2c9a3d52f2c6309fed58f9812714416
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2025-02-21 22:15:56 +0100

    refactor

--- app/test/grades/pages/grades_dialog/grades_dialog_controller_test.dart
+++ app/test/grades/pages/grades_dialog/grades_dialog_controller_test.dart
@@ -3,11 +3,9 @@ import 'package:crash_analytics/crash_analytics.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:sharezone/grades/grades_service/grades_service.dart';
 import 'package:sharezone/grades/pages/grades_dialog/grades_dialog_controller.dart';
 
-import '../../../../test_goldens/grades/pages/create_term_page/create_term_page_test.mocks.dart';
 import '../../grades_test_common.dart';
 
 class MockCrashAnalytics extends Mock implements CrashAnalytics {}
@@ -22,17 +20,21 @@ void main() {
     late Analytics analytics;
     late GradesDialogController controller;
 
-    setUp(() {
-      gradesService = GradesService();
-      gradesTestController = GradesTestController(gradesService: gradesService);
-      crashAnalytics = MockCrashAnalytics();
-      analytics = MockAnalytics();
-      controller = GradesDialogController(
+    GradesDialogController createController() {
+      return GradesDialogController(
         gradesService: gradesService,
         coursesStream: Stream.value([]),
         crashAnalytics: crashAnalytics,
         analytics: analytics,
       );
+    }
+
+    setUp(() {
+      gradesService = GradesService();
+      gradesTestController = GradesTestController(gradesService: gradesService);
+      crashAnalytics = MockCrashAnalytics();
+      analytics = MockAnalytics();
+      controller = createController();
     });
 
     test(
@@ -42,12 +44,7 @@ void main() {
           termWith(id: TermId('foo'), name: "Foo term"),
         );
 
-        controller = GradesDialogController(
-          gradesService: gradesService,
-          coursesStream: Stream.value([]),
-          crashAnalytics: crashAnalytics,
-          analytics: analytics,
-        );
+        controller = createController();
 
         expect(controller.view.selectedTerm, (
           id: TermId('foo'),
@@ -63,12 +60,7 @@ void main() {
           termWith(gradingSystem: GradingSystem.zeroToFifteenPoints),
         );
 
-        controller = GradesDialogController(
-          gradesService: gradesService,
-          coursesStream: Stream.value([]),
-          crashAnalytics: crashAnalytics,
-          analytics: analytics,
-        );
+        controller = createController();
 
         expect(
           controller.view.selectedGradingSystem,
