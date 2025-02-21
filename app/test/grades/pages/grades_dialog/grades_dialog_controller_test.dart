b47048e920b1ff73d545d3d6666fdb1c6913c6c5
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2025-02-21 22:45:56 +0100

    Add TakeIntoAccountState tests

--- app/test/grades/pages/grades_dialog/grades_dialog_controller_test.dart
+++ app/test/grades/pages/grades_dialog/grades_dialog_controller_test.dart
@@ -7,6 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sharezone/grades/grades_service/grades_service.dart';
 import 'package:sharezone/grades/pages/grades_dialog/grades_dialog_controller.dart';
+import 'package:sharezone/grades/pages/grades_dialog/grades_dialog_view.dart';
 
 import '../../grades_test_common.dart';
 
@@ -85,6 +86,69 @@ void main() {
         expect(
           controller.view.selectedGradingSystem,
           GradingSystem.oneToSixWithPlusAndMinus,
+        );
+      },
+    );
+
+    test('$TakeIntoAccountState is enabled by default', () {
+      controller = createController();
+      expect(
+        controller.view.takeIntoAccountState,
+        TakeIntoAccountState.enabled,
+      );
+    });
+
+    test(
+      '$TakeIntoAccountState is ${TakeIntoAccountState.disabledWrongGradingSystem} when grade type has weight of zero',
+      () {
+        gradesTestController.createTerm(
+          termWith(
+            gradeTypeWeights: {GradeType.presentation.id: Weight.zero},
+            subjects: [
+              subjectWith(id: SubjectId('maths'), grades: [gradeWith()]),
+            ],
+          ),
+        );
+
+        controller = createController();
+        controller.setSubject(SubjectId('maths'));
+        controller.setGradeType(GradeType.presentation);
+
+        expect(
+          controller.view.takeIntoAccountState,
+          TakeIntoAccountState.disabledGradeTypeWithNoWeight,
+        );
+      },
+    );
+
+    test(
+      '$TakeIntoAccountState is ${TakeIntoAccountState.disabledWrongGradingSystem} when term and selected grading systems differ',
+      () {
+        gradesTestController.createTerm(
+          termWith(gradingSystem: GradingSystem.zeroToFifteenPoints),
+        );
+        controller = createController();
+        controller.setGradingSystem(GradingSystem.oneToSixWithPlusAndMinus);
+
+        expect(
+          controller.view.takeIntoAccountState,
+          TakeIntoAccountState.disabledWrongGradingSystem,
+        );
+      },
+    );
+
+    test(
+      '$TakeIntoAccountState remains enabled when term and selected grading systems match',
+      () {
+        gradesTestController.createTerm(
+          termWith(gradingSystem: GradingSystem.zeroToFifteenPoints),
+        );
+        controller = createController();
+        controller.setGradingSystem(GradingSystem.zeroToFifteenPoints);
+
+        expect(
+          controller.view.takeIntoAccountState,
+          TakeIntoAccountState.enabled,
         );
       },
     );
