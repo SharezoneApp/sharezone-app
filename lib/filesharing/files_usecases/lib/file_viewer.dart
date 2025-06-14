a1cc196e814d3bb00e697d183efd0e06d95e4fc8
Nils Reichardt <me@nils.re>
2025-06-14 13:33:24 +0200

    Show error page when `downloadURL` is not available (#1880)
    <img width="1552" alt="image"
    src="https://github.com/user-attachments/assets/7cdc7fd9-dd20-4209-b327-1f9b0fb7b755"
    />

--- lib/filesharing/files_usecases/lib/file_viewer.dart
+++ lib/filesharing/files_usecases/lib/file_viewer.dart
@@ -8,7 +8,8 @@
 
 library;
 
-export 'src/file_viewer/widgets/file_page_app_bar.dart';
+export 'src/file_viewer/pages/error_file_page.dart';
 export 'src/file_viewer/pages/image_file_page.dart';
 export 'src/file_viewer/pages/pdf_file_page.dart';
 export 'src/file_viewer/pages/video_file_page.dart';
+export 'src/file_viewer/widgets/file_page_app_bar.dart';
