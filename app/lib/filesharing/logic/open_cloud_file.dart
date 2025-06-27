1797c418728abd357bf8423d8e6ae7f875a810dc
nilsreichardt <me@nils.re>
2025-06-27 23:59:49 +0200

    Improve `open_cloud_file.dart`

--- app/lib/filesharing/logic/open_cloud_file.dart
+++ app/lib/filesharing/logic/open_cloud_file.dart
@@ -74,12 +74,7 @@ void openFirestoreFilePage({
   Stream<String>? nameStream,
   String? id,
 }) {
-  if (!PlatformCheck.isWeb &&
-      (fileFormat == FileFormat.unknown ||
-          fileFormat == FileFormat.excel ||
-          fileFormat == FileFormat.text ||
-          fileFormat == FileFormat.zip ||
-          (PlatformCheck.isIOS && fileFormat == FileFormat.pdf))) {
+  if (PlatformCheck.isIOS && fileFormat == FileFormat.pdf) {
     showDialog(
       context: context,
       builder:
@@ -90,9 +85,25 @@ void openFirestoreFilePage({
             nameStream: nameStream,
           ),
     );
+    return;
   }
+
+  if (!PlatformCheck.isWeb &&
+      (fileFormat == FileFormat.unknown ||
+          fileFormat == FileFormat.excel ||
+          fileFormat == FileFormat.text ||
+          fileFormat == FileFormat.zip)) {
+    saveFileOnDevice(
+      context: context,
+      downloadUrl: downloadURL,
+      fileId: id,
+      fileName: name,
+    );
+    return;
+  }
+
   // Die Datei wird im Web gedownloadet, sollten wir keine passende Anzeige (Video/PDF/Bild) haben
-  else if (PlatformCheck.isWeb &&
+  if (PlatformCheck.isWeb &&
       !(fileFormat == FileFormat.video ||
           fileFormat == FileFormat.image ||
           fileFormat == FileFormat.pdf)) {
@@ -102,23 +113,24 @@ void openFirestoreFilePage({
       fileId: id,
       fileName: name,
     );
-  } else {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder:
-            (context) => FirestoreFilePage(
-              id: id,
-              downloadURL: downloadURL,
-              name: name,
-              nameStream: nameStream,
-              actions: actions,
-              fileFormat: fileFormat,
-            ),
-        settings: const RouteSettings(name: FirestoreFilePage.tag),
-      ),
-    );
+    return;
   }
+
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder:
+          (context) => FirestoreFilePage(
+            id: id,
+            downloadURL: downloadURL,
+            name: name,
+            nameStream: nameStream,
+            actions: actions,
+            fileFormat: fileFormat,
+          ),
+      settings: const RouteSettings(name: FirestoreFilePage.tag),
+    ),
+  );
 }
 
 class FirestoreFilePage extends StatelessWidget {
