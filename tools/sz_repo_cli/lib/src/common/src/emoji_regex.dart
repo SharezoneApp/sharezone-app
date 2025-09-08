5ba8f81b9ee0a834c1136bfe404085a124cb5c0a
nilsreichardt <me@nils.re>
2025-09-08 09:37:59 -0700

    Add copyright header

--- tools/sz_repo_cli/lib/src/common/src/emoji_regex.dart
+++ tools/sz_repo_cli/lib/src/common/src/emoji_regex.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 /// emoji-regex offers a regular expression to match all emoji symbols and
 /// sequences (including textual representations of emoji) as per the Unicode
 /// Standard.
