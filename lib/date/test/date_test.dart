aaf2d65f6007259832994a8693eba051e24483ef
nilsreichardt <me@nils.re>
2025-10-11 19:51:43 -0700

    Add more tests

--- lib/date/test/date_test.dart
+++ lib/date/test/date_test.dart
@@ -4,9 +4,13 @@ import 'package:test/test.dart';
 void main() {
   group(Date, () {
     test('compareTo', () {
-      final today = Date('2025-10-11');
-      final tomorrow = today.addDays(1);
-      expect(today.compareTo(tomorrow), -1);
+      final date = Date('2025-10-11');
+      final sameDate = Date('2025-10-11');
+      final nextDate = date.addDays(1);
+
+      expect(date.compareTo(nextDate), isNegative);
+      expect(nextDate.compareTo(date), isPositive);
+      expect(date.compareTo(sameDate), isZero);
     });
   });
 }
