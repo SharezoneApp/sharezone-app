050f922c2f780dce2292d852ed6d5750900f1c46
nilsreichardt <me@nils.re>
2026-01-30 15:09:18 +0100

    Add license header

--- docs/eslint.config.mjs
+++ docs/eslint.config.mjs
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2026 Sharezone UG (haftungsbeschränkt)
+ * Licensed under the EUPL-1.2-or-later.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
 export default [
   {
     ignores: [".next/**", "node_modules/**", "out/**"]
