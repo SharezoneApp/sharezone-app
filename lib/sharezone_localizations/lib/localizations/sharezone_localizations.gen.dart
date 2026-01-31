ad19ffd6fec832e997b811ed7c1d72e4c219a78e
Nils Reichardt <me@nils.re>
2026-01-31 11:48:06 +0100

    Update license header in sharezone_locations files (#2025)
    Required for https://github.com/SharezoneApp/sharezone-app/pull/2023

--- lib/sharezone_localizations/lib/localizations/sharezone_localizations.gen.dart
+++ lib/sharezone_localizations/lib/localizations/sharezone_localizations.gen.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Sharezone UG (haftungsbeschränkt)
+// Copyright (c) 2026 Sharezone UG (haftungsbeschränkt)
 // Licensed under the EUPL-1.2-or-later.
 //
 // You may obtain a copy of the Licence at:
