74080d865661ce38765a32cf3ceae063971e1b9b
nilsreichardt <me@nils.re>
2026-02-07 10:20:52 +0100

    Add lh

--- lib/key_value_store/lib/key_value_store.dart
+++ lib/key_value_store/lib/key_value_store.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2026 Sharezone UG (haftungsbeschränkt)
+// Licensed under the EUPL-1.2-or-later.
+//
+// You may obtain a copy of the Licence at:
+// https://joinup.ec.europa.eu/software/page/eupl
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 export 'src/key_value_store.dart';
 export 'src/flutter_key_value_store.dart';
 export 'src/in_memory_key_value_store.dart';
